45853c7e00d1952fffc4545388093dab417a688b
Dev Testing SeamlessCMS <cmstest@ubuntu>
2020-05-25 10:44:32 -0400

    Completed create new content type

--- admin/controllers/content/views/new/model.php
+++ admin/controllers/content/views/new/model.php
@@ -34,6 +34,79 @@ if ($new_content_type_form->is_submitted()) {
 		// then create folder - if done
 		// then create files, view folde, view files
 		// then reload content page and allow auto-installer to do its thing :)
+		if (file_exists(CMSPATH . "/controllers/" . $foldername)) {
+			CMS::Instance()->queue_message('Folder for new controller type already exists','error',Config::$uripath . '/admin/content/new');
+		}
+		if (!mkdir(CMSPATH . "/controllers/" . $foldername)) {
+			CMS::Instance()->queue_message('Unable to create folder for new controller type','error',Config::$uripath . '/admin/content/new');
+		}
+
+		// folder created
+		// now create files
+
+		$custom_fields_dot_json = "
+		{
+			\"title\":\"{$title}\",
+			\"id\":\"{$foldername}\",
+			\"fields\":[
+			]
+		}";
+		if (!file_put_contents (CMSPATH . '/controllers/' . $foldername . "/custom_fields.json", $custom_fields_dot_json)) {
+			CMS::Instance()->queue_message('Unable to create custom_fields.json file for new controller type','error',Config::$uripath . '/admin/content/new');
+		}
+
+		$controller_config_dot_json = "
+		{
+			\"title\":\"{$title}\",
+			\"description\":\"{$description}\"
+		}
+		";
+		if (!file_put_contents (CMSPATH . '/controllers/' . $foldername . "/controller_config.json", $controller_config_dot_json)) {
+			CMS::Instance()->queue_message('Unable to create custom_fields.json file for new controller type','error',Config::$uripath . '/admin/content/new');
+		}
+
+		// copy controller.php (no custom text required)
+		if (!copy(ADMINPATH . '/controllers/content/views/new/new_controller_files/' . "controller.php", CMSPATH . "/controllers/" . $foldername . "/controller.php")) {
+			CMS::Instance()->queue_message('Unable to copy controller.php file for new controller type default view','error',Config::$uripath . '/admin/content/new');
+		}
+
+		// create default view
+
+		// make views folders
+		if (!mkdir(CMSPATH . "/controllers/" . $foldername . "/views")) {
+			CMS::Instance()->queue_message('Unable to create views folder for new controller type','error',Config::$uripath . '/admin/content/new');
+		}
+		if (!mkdir(CMSPATH . "/controllers/" . $foldername . "/views/" . $defaultviewfoldername)) {
+			CMS::Instance()->queue_message('Unable to create views default view folder for new controller type','error',Config::$uripath . '/admin/content/new');
+		}
+
+		// files to be placed inside view/defaultview
+
+		// copy options_form.json, model.php, and view.php - warn that changes should be made to these files
+
+		if (!copy(ADMINPATH . '/controllers/content/views/new/new_controller_files/' . "options_form.json", CMSPATH . "/controllers/" . $foldername . "/views/" . $defaultviewfoldername . "/options_form.json")) {
+			CMS::Instance()->queue_message('Unable to copy options_form.json file for new controller type default view','error',Config::$uripath . '/admin/content/new');
+		}
+		if (!copy(ADMINPATH . '/controllers/content/views/new/new_controller_files/' . "model.php", CMSPATH . "/controllers/" . $foldername . "/views/" . $defaultviewfoldername . "/model.php")) {
+			CMS::Instance()->queue_message('Unable to copy model.php file for new controller type default view','error',Config::$uripath . '/admin/content/new');
+		}
+		if (!copy(ADMINPATH . '/controllers/content/views/new/new_controller_files/' . "view.php", CMSPATH . "/controllers/" . $foldername . "/views/" . $defaultviewfoldername . "/view.php")) {
+			CMS::Instance()->queue_message('Unable to copy view.php file for new controller type default view','error',Config::$uripath . '/admin/content/new');
+		}
+
+		$view_configuration_dot_json = "
+		{
+			\"title\":\"{$defaultviewtitle}\",
+			\"description\":\"{$defaultviewdescription}\"
+		}
+		";
+		if (!file_put_contents (CMSPATH . '/controllers/' . $foldername . "/views/" . $defaultviewfoldername . "/view_configuration.json", $view_configuration_dot_json)) {
+			CMS::Instance()->queue_message('Unable to create view_configuration.json file for new controller type default view','error',Config::$uripath . '/admin/content/new');
+		}
+
+		// got here - all good :)
+		CMS::Instance()->queue_message('New content type created - importing','success',Config::$uripath . '/admin/content/');
+		
 	}
 	else {
 		echo "<h2>It's INVALID!</h2>";
