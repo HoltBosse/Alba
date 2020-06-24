11a65214a22f9902bb8919734c3a89193c616c3d
Dev Playground <play@ubuntu>
2020-06-23 23:03:43 -0400

    add setupfiles.sh for quick lamp setup

---
+++ setupfiles.sh
@@ -0,0 +1,7 @@
+# quickly setup files for apache/lamp
+# make this file executable
+# chmod +x setupfiles.sh ;)
+# ./setupfiles.sh to run
+cp configTESTBACKUP.php config.php
+cp htaccess.txt .htaccess
+cp .htaccess ./admin/.htaccess
