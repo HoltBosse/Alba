088dff7f3d5a3b8cb951d1ac597913466dcba80a
Dev Playground <play@ubuntu>
2020-06-25 12:58:54 -0400

    Update version

--- core/cms.php
+++ core/cms.php
@@ -27,7 +27,7 @@ final class CMS {
 	private static $instance = null;
 	private $core_controller = false;
 	private $need_session = true;
-	public $version = "0.18 (beta) - default template";
+	public $version = "0.19 (beta) - repeatable field";
 
 	/* protected function __construct() {}
     protected function __clone() {}
