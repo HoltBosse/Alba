2cbf3b43b6d2c32d63afcb261be8a02da9ba9a73
Dev Testing SeamlessCMS <cmstest@ubuntu>
2020-10-17 23:12:17 -0400

    update version to 0.22

--- core/cms.php
+++ core/cms.php
@@ -29,7 +29,7 @@ final class CMS {
 	private $core_controller = false;
 	private $need_session = true;
 	public $hooks = [];
-	public $version = "0.2";
+	public $version = "0.22";
 
 	/* protected function __construct() {}
     protected function __clone() {}
