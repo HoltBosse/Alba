de80e3c5a395c45e950c738ae41951d4c2bceecf
Dev Testing SeamlessCMS <cmstest@ubuntu>
2020-10-18 10:36:16 -0400

    admin user menu lists groups to show

--- admin/controllers/users/views/default/model.php
+++ admin/controllers/users/views/default/model.php
@@ -3,5 +3,22 @@ defined('CMSPATH') or die; // prevent unauthorized access
 
 // any variables created here will be available to the view
 
+
+$group_id = NULL;
+$group_name = "All";
+$segments = CMS::Instance()->uri_segments;
+if (sizeof($segments)==2) {
+	if (is_numeric($segments[1])) {
+        $group_id = $segments[1];
+        $group_name = User::get_group_name ($group_id);
+    }
+}
+
 $user = new User();
-$all_users = $user->get_all_users();
+
+if ($group_id) {
+    $all_users = $user->get_all_users_in_group($group_id);
+}
+else {
+    $all_users = $user->get_all_users();
+}
