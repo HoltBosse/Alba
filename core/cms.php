b468f27af3ab7f396058b25af163dd0384aa52a1
Bob CMS Test <bobmitch@gmail.com>
2021-01-08 23:41:01 -0500

    version update

--- core/cms.php
+++ core/cms.php
@@ -29,7 +29,7 @@ final class CMS {
 	private $core_controller = false;
 	private $need_session = true;
 	public $hooks = [];
-	public $version = "0.24";
+	public $version = "0.25";
 
 	/* protected function __construct() {}
     protected function __clone() {}
