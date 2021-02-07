5051c97a1c7f443e7f104bd1abe4d99ea3ad0858
Bob CMS Test <bobmitch@gmail.com>
2021-02-06 19:00:13 -0500

    Tab field added

--- admin/templates/clean/js/script.js
+++ admin/templates/clean/js/script.js
@@ -96,6 +96,55 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// TABS
+
+
+function getTabIndex(el) {
+	return [...el.parentElement.children].indexOf(el);	
+}
+
+function deactivateAllTabs() {
+	alltabs = document.querySelectorAll('.tabs li, .tab-content')
+	alltabs.forEach(function (tab) {
+		tab.classList.remove('is-active');
+	});
+}
+
+let alltabs = document.querySelectorAll('.tabs');
+alltabs.forEach(tabs => {
+	let closest_wrap = tabs.closest('.tabs-wrap');
+	let content_wrap = closest_wrap.querySelector('.tab-content-start');
+	// set first tab active
+	tabs.querySelector('li').classList.add('is-active');
+	// set first content active
+	content_wrap.querySelector('.tab-content').classList.add('is-active');
+	console.log(content_wrap);
+	// click event handler for tab headings
+	tabs.querySelectorAll('li').forEach(tab => {
+		tab.addEventListener('click',function(e){
+			e.preventDefault();
+			// remove active class from all current tabset active elements
+			let all_active = closest_wrap.querySelectorAll('.is-active');
+			all_active.forEach(active => {
+				active.classList.remove('is-active');
+			});
+			let index = getTabIndex(e.target.closest('li'));
+			tabs.querySelectorAll('li')[index].classList.add('is-active');
+			let all_tab_contents = content_wrap.querySelectorAll('.tab-content');
+			if (all_tab_contents.length<=index) {
+				// form might be incomplete with more tab headings than content areas
+				// fail silently
+				console.log('missing tab content area - check form json');
+			}
+			else {
+				// activate content
+				content_wrap.querySelectorAll('.tab-content')[index].classList.add('is-active');
+			}
+		});
+	});
+});
+
+// END TABS
 
 /* LAZYLOAD IMAGES */
 
