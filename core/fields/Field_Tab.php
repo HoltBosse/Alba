fcc402752b419ff5dedc1e842cda51b4c492fa09
Bob CMS Test <bobmitch@gmail.com>
2021-02-06 19:35:33 -0500

    Updated general options form to use new tab form field

--- core/fields/Field_Tab.php
+++ core/fields/Field_Tab.php
@@ -16,7 +16,7 @@ class Field_Tab extends Field {
 		if (property_exists($this,'mode')) {
 			if ($this->mode=="tabs"):?>
 			<div id="<?php echo $this->id;?>_tabs_wrap" class='tabs-wrap'>
-				<div id="<?php echo $this->id;?>" class="tabs">
+				<div id="<?php echo $this->id;?>" class="tabs is-boxed">
 					<ul>
 						<?php foreach ($this->tabs as $tab): ?>
 							<li><a><?php echo $tab; ?></a></li>
