910ee34ced0acd9f587210e7dc3f5059f0753b65
Bob CMS Test <bobmitch@gmail.com>
2021-02-13 11:16:58 -0500

    Simplify blog article link generation - works for homepage blog now

--- controllers/basic_article/views/blog/view.php
+++ controllers/basic_article/views/blog/view.php
@@ -12,7 +12,7 @@ defined('CMSPATH') or die; // prevent unauthorized access
 		<hr>
 		<h4 class='title is-4'><?php echo $blog->title; ?>
 		<h5 class='title is-5'><?php echo date("F jS, Y", strtotime($blog->start));?>
-		<a class='readmore' href='<?php echo Config::$uripath . '/' . implode('/',CMS::Instance()->uri_path_segments) . '/' . $blog->alias;?>'>Read More</a>
+		<a class='readmore' href='<?php echo CMS::Instance()->page->get_url() . "/" . $blog->alias;?>'>Read More</a>
 		<?php //echo $blog->f_markup; ?>
 		<hr>
 	<?php endforeach; ?>
