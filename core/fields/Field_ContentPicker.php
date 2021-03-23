99cc3d5032880745d08a0cd6ef8331955983143b
Bob CMS Test <bobmitch@gmail.com>
2021-03-23 15:06:25 -0400

    contentpicker field now does something

--- core/fields/Field_ContentPicker.php
+++ core/fields/Field_ContentPicker.php
@@ -2,35 +2,98 @@
 defined('CMSPATH') or die; // prevent unauthorized access
 
 class Field_ContentPicker extends Field {
+
+	public $select_options;
+
+	function __construct($tagid="") {
+		$this->id = "";
+		$this->name = "";
+		$this->select_options=[];
+		$this->default = "";
+		$this->content_type="";
+		$this->type = "ContentPicker";
+	}
+
+	
+
 	public function display() {
-		echo "<label class='label'>Content Picker</label>";
-		echo "<p>Hello, I am a content picker!</p>";
+		$this->array_values = json_decode ($this->default);
+		$required="";
+		if ($this->content_type) {
+			// get_all_content($order_by="id", $type_filter=false, $id=null, $tag=null, $published_only=null, $list_fields=[], $ignore_fields=[], $filter_field=null, $filter_val=null, $page=0) 
+			$content = Content::get_all_content ("id", $this->content_type, null, null, true); // get all published only content
+		}
+		else {
+			CMS::show_error('ContentPicker must have content type specified');
+		}
+		if ($this->required) {$required=" required ";}
+		// if id needs to be unique for scripting purposes, make sure replacement text inserted
+		// this will be replaced during repeatable template literal js injection when adding new
+		// repeatable form item
+		if ($this->in_repeatable_form===null) {
+			$repeatable_id_suffix='';
+		}
+		else {
+			$repeatable_id_suffix='{{repeatable_id_suffix}}';
+		}
+		echo "<div class='field'>";
+			echo "<label class='label'>" . $this->label . "</label>";
+			echo "<div class='control'>";
+				echo "<div class='select is-multiple'>";
+					echo "<select class='is-multiple' multiple {$required} id='{$this->id}{$repeatable_id_suffix}' {$this->get_rendered_name(true)}>";
+						if ($this->required) {
+							echo "<option value='' >{$this->label}</option>";
+						}
+						foreach ($content as $item) {
+							if ($item->state==1) {
+								$selected = "";
+								if (in_array($item->id, $this->array_values)) { $selected="selected";}
+								echo "<option {$selected} value='{$item->id}'>{$item->title}</option>";
+							}
+						}
+					echo "</select>";
+				echo "</div>";
+			echo "</div>";
+		echo "</div>";
+		if ($this->description) {
+			echo "<p class='help'>" . $this->description . "</p>";
+		}
+		// Slimselect Multiple library 
+		if ($this->in_repeatable_form===null) {
+			echo "<script>new SlimSelect({ select: '#{$this->id}' });</script>"; 
+		}
+		else {
+			// also inject id_suffix to be replace at injection time
+			echo "<script>new SlimSelect({ select: '#{$this->id}{$repeatable_id_suffix}' });</script>"; 
+		}
 	}
 
-	public function inject_designer_javascript() {
-		?>
-		<script>
-			window.Field_ContentPicker = {};
-			// template is what gets injected when the field 'insert new' button gets clicked
-			window.Field_ContentPicker_template = `
-			<div class="field">
-				<h2 class='heading title'>Rich/HTML Field</h2>	
 
-				<label class="label">Label</label>
-				<div class="control has-icons-left has-icons-right">
-					<input required name="label" class="input iss-success" type="label" placeholder="Label" value="">
-				</div>
 
-				<label class="label">Required</label>
-				<div class="control has-icons-left has-icons-right">
-					<input name="required" class="checkbox iss-success" type="checkbox"  value="">
-				</div>
-			</div>`;
-		</script>
-		<?php 
+	public function load_from_config($config) {
+		$this->name = $config->name ?? 'error!!!';
+		$this->id = $config->id ?? $this->name;
+		$this->label = $config->label ?? '';
+		$this->required = $config->required ?? false;
+		$this->description = $config->description ?? '';
+		$this->filter = $config->filter ?? 'ARRAYOFINT';
+		$this->missingconfig = $config->missingconfig ?? false;
+		$this->select_options = $config->select_options ?? [];
+		$this->default = json_decode($config->default) ?? "";
+		$this->type = $config->type ?? 'error!!!';
+		$this->content_type = $config->content_type ?? false;
 	}
 
-	public function designer_display() {
+	public function get_friendly_value() {
+		$stmt = CMS::Instance()->pdo->prepare('select title from content where id=?');
+		$stmt->execute(array($this->default));
+		return $stmt->fetch()->title;
+	}
 
+	public function validate() {
+		if ($this->is_missing()) {
+			return false;
+		}
+		return true;
 	}
 }
