6028e243cfcd3d0324b1663bc6ce96ce14e26ebf
Bob CMS Test <bobmitch@gmail.com>
2021-04-02 11:38:53 -0400

    menu widget auto id

--- widgets/menu/widget_class.php
+++ widgets/menu/widget_class.php
@@ -211,7 +211,8 @@ class Widget_menu extends Widget {
 		//echo json_last_error_msg();
 		//CMS::pprint_r ($this_menu_structure);
 		//echo "<code>Menu Widget</code>";
-		echo "<div class='tree_wrap'>";
+		
+		echo "<div id='menu_" . Input::make_alias($this->title) . "' class='tree_wrap'>";
 		$this->render_menu($this_menu_structure);
 		echo "</div>";
 	}
