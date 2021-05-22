6e557b84eb326394a8ac40b07bf5b9913031f555
Bob CMS Test <bobmitch@gmail.com>
2021-05-22 12:43:09 -0400

    fix saveable content list bug

--- core/cms.php
+++ core/cms.php
@@ -29,7 +29,7 @@ final class CMS {
 	private $core_controller = false;
 	private $need_session = true;
 	public $hooks = [];
-	public $version = "0.365";
+	public $version = "0.366";
 
 	/* protected function __construct() {}
     protected function __clone() {}
