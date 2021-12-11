18886c2b04ec718bbf9fb56749ae39ce894a0b77
Bob CMS Test <bobmitch@gmail.com>
2021-12-11 04:05:53 +0000

    working well

--- admin/controllers/images/views/show/model.php
+++ admin/controllers/images/views/show/model.php
@@ -7,6 +7,7 @@ $all_images = CMS::Instance()->pdo->query($query)->fetchAll();
 $image_tags = Content::get_applicable_tags ("-1");
 
 $filter = Input::getvar('filter','STRING');
+$autoclose = Input::getvar('autoclose','STRING');
 
 $max_upload_size = File::get_max_upload_size();
 $max_upload_size_bytes = File::get_max_upload_size_bytes();
