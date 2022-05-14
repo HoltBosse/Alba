ea3bb026706389929140650d652d1ea6e2e96635
Bob CMS Test <bobmitch@gmail.com>
2022-05-14 13:23:00 +0000

    repeatable displays saved info

--- admin/controllers/home/views/default/model.php
+++ admin/controllers/home/views/default/model.php
@@ -4,14 +4,14 @@ defined('CMSPATH') or die; // prevent unauthorized access
 // any variables created here will be available to the view
 
 $test_form = new Form();
-//$test_form->load_json(); // load test form
+$test_form->load_json(); // load test form
 
 if ($test_form->is_submitted()) {
 	//CMS::pprint_r ($test_form);
 	$test_form->set_from_submit();
 	if ($test_form->validate()) {
-		//CMS::pprint_r ($test_form);
-		//echo "<h2>It's valid!</h2>";
+		CMS::pprint_r ($test_form);
+		echo "<h2>It's valid!</h2>";
 		$json = $test_form->serialize_json();
 		CMS::pprint_r ($json);
 	}
