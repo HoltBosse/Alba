2e749376049f9d41ba3ac77a2261b4b4b4641aaf
Bob CMS Test <bobmitch@gmail.com>
2022-07-24 14:37:12 +0000

    change tag description to medium text

--- installer/schema.sql
+++ installer/schema.sql
@@ -109,7 +109,7 @@ CREATE TABLE `tags` (
   `alias` varchar(255) NOT NULL,
   `image` int(11) DEFAULT NULL,
   `filter` int(11) NOT NULL DEFAULT '1' COMMENT '0 admin only 1 exclusive 2 inclusive',
-  `description` varchar(255) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
   `note` varchar(255) DEFAULT NULL,
   `parent` int(11) DEFAULT NULL,
   `category` int(11) NOT NULL DEFAULT 0
