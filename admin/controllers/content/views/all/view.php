c62728c2447bae878c4f33a6fc0f62e71ca07bae
Bob CMS Test <bobmitch@gmail.com>
2022-07-30 23:38:41 +0000

    url filter params preserved during pagination

--- admin/controllers/content/views/all/view.php
+++ admin/controllers/content/views/all/view.php
@@ -351,20 +351,39 @@ CMS::pprint_r ($pagination_size);
 CMS::pprint_r ($order_by);
 CMS::pprint_r (sizeof($all_content)); */
 $num_pages = ceil($content_count/$pagination_size);
+
+//$url_query_params = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+$url_query_params = $_GET;
+$url_path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+if ($cur_page) {
+	// not ordering view and page url is either 1 or no passed and assumed to be 1 in model
+	$url_query_params['page'] = $cur_page+1;
+	$next_url_params = http_build_query($url_query_params);
+	$url_query_params['page'] = $cur_page-1;
+	$prev_url_params = http_build_query($url_query_params);
+	/* CMS::pprint_r ($url_query_params);
+	CMS::pprint_r ($next_url_params); */
+}
+
 ?>
 
 <?php if ($content_count>$pagination_size && !$order_by):?>
 <nav class="pagination is-centered" role="navigation" aria-label="pagination">
 	<?php if ($cur_page>1):?>
-		<a href='?page=<?php echo $cur_page-1;?><?php if ($search) { echo "&search=" . $search; }?>' class="pagination-previous">Previous</a>
+		<a href='<?=$url_path . "?" . $prev_url_params;?>' class="pagination-previous">Previous</a>
 	<?php endif;?>
 	<?php if ( ($content_count>sizeof($all_content)) && !$order_by && ( ($cur_page*$pagination_size)<$content_count ) ):?>
-		<a href='?page=<?php echo $cur_page+1;?><?php if ($search) { echo "&search=" . $search; }?>' class="pagination-next">Next page</a>
+		<a href='<?=$url_path . "?" . $next_url_params;?>' class="pagination-next">Next page</a>
 	<?php endif; ?>
 	<ul class="pagination-list">
 		<?php for ($n=1; $n<=$num_pages; $n++):?>
-		<li>
-			<a class='pagination-link <?php if ($n==$cur_page) {echo "is-current";}?>' href='?page=<?php echo $n;?><?php if ($search) { echo "&search=" . $search; }?>'><?php echo $n;?></a>
+			<?php 
+			$url_query_params['page'] = $n;
+			$url_params = http_build_query($url_query_params);
+			?>
+		<li> 
+			<a class='pagination-link <?php if ($n==$cur_page) {echo "is-current";}?>' href='<?=$url_path . "?" . $url_params?>'><?php echo $n;?></a>
 		</li>
 		<?php endfor; ?>
 	</ul>
