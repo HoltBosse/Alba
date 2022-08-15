82f0e73229a7b1f6b5cfa6c51e848145b990eafc
bobmitch <bobmitch@gmail.com>
2022-08-14 20:34:34 -0400

    chore: version bump to 2.4.74

--- core/cms.php
+++ core/cms.php
@@ -30,7 +30,7 @@ final class CMS {
 	private $core_controller = false;
 	private $need_session = true;
 	public $hooks = [];
-	public $version = "2.4.73";
+	public $version = "2.4.74";
 
 	/* protected function __construct() {}
     protected function __clone() {}
