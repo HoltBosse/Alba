7e13dd6da09f811994b255fb0b564190a557c7da
Bob CMS Test <bobmitch@gmail.com>
2022-08-23 17:44:34 +0000

    chore: version 2.4.75

--- core/cms.php
+++ core/cms.php
@@ -30,7 +30,7 @@ final class CMS {
 	private $core_controller = false;
 	private $need_session = true;
 	public $hooks = [];
-	public $version = "2.4.74";
+	public $version = "2.4.75";
 
 	/* protected function __construct() {}
     protected function __clone() {}
