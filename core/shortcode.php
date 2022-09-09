6fdac151cde96946f7580625b61789d847c72ed0
Bob CMS Test <bobmitch@gmail.com>
2022-09-09 00:30:20 +0000

    feat: simplify code and fix array issue

--- core/shortcode.php
+++ core/shortcode.php
@@ -20,12 +20,13 @@ class Shortcode {
     }
 
     function shortcode_parse_attributes($attributes_text) {
-        // TODO: clean room implementation
         $attributes = [];
         $pattern = Shortcode::get_shortcode_attributes_regex();
-        $attributes_text = preg_replace( "/[\x{00a0}\x{200b}]+/u", ' ', $attributes_text );
-        if ( preg_match_all( $pattern, $attributes_text, $match, PREG_SET_ORDER ) ) {
-
+        // remove zero width spaces if present 
+        // see: http://stackoverflow.com/questions/11305797/remove-zero-width-space-characters-from-a-javascript-string
+        $attributes_text = preg_replace( "/[\x{00a0}\x{200b}]+/u", ' ', $attributes_text);
+        // @author: wordpress
+        if (preg_match_all( $pattern, $attributes_text, $match, PREG_SET_ORDER)) {
             foreach ( $match as $m ) {
                 if ( ! empty( $m[1] ) ) {
                     $attributes[ strtolower( $m[1] ) ] = stripcslashes( $m[2] );
@@ -41,56 +42,10 @@ class Shortcode {
                     $attributes[] = stripcslashes( $m[9] );
                 }
             }
-    
-            // Reject any unclosed HTML elements.
-            foreach ( $attributes as &$value ) {
-                if ( false !== strpos( $value, '<' ) ) {
-                    if ( 1 !== preg_match( '/^[^<]*+(?:<[^>]*+>[^<]*+)*+$/', $value ) ) {
-                        $value = '';
-                    }
-                }
-            }
         } else {
-            $attributes = ltrim( $text );
+            $attributes = ltrim($text);
         }
         return $attributes;
-    }
-
-    public static function get_shortcode_regex($tagnames=null) {
-        if (!$tagnames) {
-            // none passed, play safe and get all registered
-            $tagnames = CMS::Instance()->shortcode_tags;
-        }
-        // TODO: clean room implementation
-        $tagregexp = implode( '|', array_map( 'preg_quote', $tagnames ) );
-        return '\\['                             // Opening bracket.
-            . '(\\[?)'                           // 1: Optional second opening bracket for escaping shortcodes: [[tag]].
-            . "($tagregexp)"                     // 2: Shortcode name.
-            . '(?![\\w-])'                       // Not followed by word character or hyphen.
-            . '('                                // 3: Unroll the loop: Inside the opening shortcode tag.
-            .     '[^\\]\\/]*'                   // Not a closing bracket or forward slash.
-            .     '(?:'
-            .         '\\/(?!\\])'               // A forward slash not followed by a closing bracket.
-            .         '[^\\]\\/]*'               // Not a closing bracket or forward slash.
-            .     ')*?'
-            . ')'
-            . '(?:'
-            .     '(\\/)'                        // 4: Self closing tag...
-            .     '\\]'                          // ...and closing bracket.
-            . '|'
-            .     '\\]'                          // Closing bracket.
-            .     '(?:'
-            .         '('                        // 5: Unroll the loop: Optionally, anything between the opening and closing shortcode tags.
-            .             '[^\\[]*+'             // Not an opening bracket.
-            .             '(?:'
-            .                 '\\[(?!\\/\\2\\])' // An opening bracket not followed by the closing shortcode tag.
-            .                 '[^\\[]*+'         // Not an opening bracket.
-            .             ')*+'
-            .         ')'
-            .         '\\[\\/\\2\\]'             // Closing shortcode tag.
-            .     ')?'
-            . ')'
-            . '(\\]?)';                          // 6: Optional second closing brocket for escaping shortcodes: [[tag]].
     }
 
     public static function exec_all($contents) {
@@ -109,14 +64,17 @@ class Shortcode {
         if (!$required_shortcode_tags) {
             return $contents;
         }
-        $pattern = Shortcode::get_shortcode_regex($required_shortcode_tags);
+
+        $tagregexp = implode('|', array_map('preg_quote', $required_shortcode_tags));
+        // regex @author: wordpress
+        $pattern = '\\[(\[?)(' . $tagregexp . ')(?![\w-])([^\]\/]*(?:\/(?!\])[^\]\/]*)*?)(?:(\/)\]|\](?:([^\[]*+(?:\[(?!\/\2\])[^\[]*+)*+)\[\/\2\])?)(\]?)'; 
 	    $contents = preg_replace_callback( "/$pattern/", 'Shortcode::apply_shortcode', $contents );
         return $contents;
     }
 
     public static function apply_shortcode($match) {
         // escape double [[ ]]
-        if ( '[' === $match[1] && ']' === $match[6] ) {
+        if ('['===$match[1] && ']'===$match[6]) {
             return substr( $match[0], 1, -1 );
         }
         $title = $match[2];
@@ -127,7 +85,7 @@ class Shortcode {
     }
 }
 
-// test shortcode
+// core image shortcode
 $image_shortcode = new Shortcode("image", function($shortcode_content, $attributes, $title){
     $img = new Image($attributes['id']);
     if ($img) {
