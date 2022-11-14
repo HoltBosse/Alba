20568d84c6c6efa3a98a46643dcfde04d82d7695
workaj <109080500+workaj@users.noreply.github.com>
2022-11-14 10:39:44 -0500

    bug: fix missing variable def

--- admin/controllers/categories/views/all/model.php
+++ admin/controllers/categories/views/all/model.php
@@ -14,3 +14,4 @@ if (sizeof($segments)==3) {
 $all_categories = Category::get_all_categories_by_depth($content_type_filter);
 $all_content_types = Content::get_all_content_types();
 $category_count = Category::get_category_count($content_type_filter, $search);
+$pagination_size = Configuration::get_configuration_value ('general_options', 'pagination_size');
