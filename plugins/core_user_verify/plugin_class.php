a09d87c6ecdf31bb91623031b33bf0e328e3e03e
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2022-12-19 12:10:03 -0500

    feat: adds fancier user verification email (#250)
    * feat: adds fancier user verification email
    
    * fix: adds colors null case fix

--- plugins/core_user_verify/plugin_class.php
+++ plugins/core_user_verify/plugin_class.php
@@ -20,12 +20,71 @@ class Plugin_core_user_verify extends Plugin {
     }
 
     public function send_email($email, $username, $key, $redirect) {
+        
         $mail = new Mail();
         $mail->addAddress($email,$username);
-        $mail->subject = "User Verification Details for " . Config::sitename();
+        $mail->subject = "Verify Your " . Config::sitename() . " Account";
         $mail->html = "<p>Hello, You have been registered for an account on " . Config::sitename() . "</p>";
-        $mail->html .= "<p>please click here to verify your account: <a href='https://" . ($redirect ?? $_SERVER['SCRIPT_URL']) . "?key=" . $key . "'>Verify</a></p>";
-        $mail->html .= "<p>If you believe this message to be in error or have not signed up for/requested an account, no further action is required</p>";
+
+        // get logos that might be used
+        $custom_logo_id = $this->get_option("logo_image");
+        $admin_logo_id = Configuration::get_configuration_value('general_options','admin_logo');
+
+        // identify which logo to use
+        $site_domain = "https://" . $_SERVER['SERVER_NAME'];
+        if ($custom_logo_id) {
+            $logo_src = $site_domain . "/image/" . $custom_logo_id;
+        }
+        else if ($admin_logo_id) {
+            $logo_src = $site_domain . "/image/" . $admin_logo_id;
+        }
+        else {
+            $logo_src = $site_domain . "/admin/templates/clean/alba_logo.webp";
+        }
+        
+        // set link and cta colors
+        $config_link_color = $this->get_option("email_link_color") ?? "#3e8ed0";
+        $link_color = $config_link_color == "" ? "#3e8ed0" : $config_link_color;
+        $config_cta_color = $this->get_option("email_cta_color") ?? "#3e8ed0";
+        $cta_color = $config_cta_color == "" ? "#3e8ed0" : $config_cta_color;
+
+        $mail->html = "
+            <div style='font-family: BlinkMacSystemFont, -apple-system, \"Segoe UI\", Roboto, Oxygen, Ubuntu, Cantarell, \"Fira Sans\", \"Droid Sans\", \"Helvetica Neue\", Helvetica, Arial, sans-serif; font-size: 16px; padding: 0; margin: 0;'>
+                <table style=\"padding: 0; margin: 0; border-spacing: 0px; background-color: lightgrey;\">
+                    <tbody>
+                        <tr style=\"height: 25px;\"></tr>
+                        <tr>
+                            <td style=\"width: 50px;\"></th>
+                            <td style=\"width: 500px; padding: 25px; background-color: white; border-radius: 0px;\">
+                                <br>
+                                <div style=\"text-align: center;\">
+                                    <a href=\"" . $site_domain . "\">
+                                        <img src=\"" . $logo_src . "\" alt=\"" . Config::sitename() . " Logo\" style=\"width: 300px;\">
+                                    </a>
+                                </div>
+                                <br>
+                                <div style=\"text-align: left; font-weight: normal;\">
+                                    <h1 style='text-align: center;  font-size: 24px;'>
+                                        Verify Your Account
+                                    </h1>
+                                    <p>Your email was just used to register for an account with <a href=\"https://organizationalorbits.org\" style=\"color: " . $link_color . "\">" . Config::sitename() . '</a>. If this was you:</p>
+                                    <br>
+                                    <div style="text-align: center;">
+                                        <a style="text-align: center;  text-decoration: none;  background-color: ' . $cta_color . '; color: white; padding: 10px 15px; border-radius: 5px; font-size: 16px;" href="https://' . ($redirect ?? $_SERVER['SCRIPT_URL']) . "?key=" . $key . "\">Verify Your Account</a>
+                                    </div>
+                                    <br>
+                                    <p>If you did not register for an " . Config::sitename() . " account <span style=\"font-weight: bold;\">do not</span> click the button above. Your email will not be associated with any account. No further action is needed.</p>
+                                    <br>
+                                    <br>
+                                </div>
+                            </th>
+                            <td style=\"width: 50px; \"></th>
+                        </tr>
+                        <tr style=\"height: 25px;\"></tr>
+                    </tbody>
+                </table>
+            </div>    
+        ";
         $mail->send();
     }
 
