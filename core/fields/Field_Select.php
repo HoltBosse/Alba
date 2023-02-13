8f3853702215ca3590b906dc29ea65e2dd3cc7eb
workaj <109080500+workaj@users.noreply.github.com>
2023-02-13 14:15:35 +0000

    feat: switch to actually decoding data

--- core/fields/Field_Select.php
+++ core/fields/Field_Select.php
@@ -43,7 +43,7 @@ class Field_Select extends Field {
 						}
 						foreach ($this->select_options as $select_option) {
 							$selected = "";
-							if ($this->multiple && $select_option->value != "" && str_contains($this->default, $select_option->value)) {
+							if ($this->multiple && in_array($select_option->value, json_decode($this->default))) {
 								$selected="selected";
 							} elseif ($select_option->value == $this->default) {
 								$selected="selected";
