bf821feef7d6e818079411de990e0f233b447f9a
workaj <109080500+workaj@users.noreply.github.com>
2023-04-05 08:31:11 -0400

    feat: add novel concept of actions for categories (#279)
    * feat: add novel concept of actions for categories
    
    * bug: make show_error call numeric

---
+++ admin/controllers/categories/views/action/model.php
@@ -0,0 +1,56 @@
+<?php
+defined('CMSPATH') or die; // prevent unauthorized access
+
+$action = CMS::Instance()->uri_segments[2];
+if (!$action) {
+	CMS::Instance()->queue_message('Unknown action','danger', $_SERVER['HTTP_REFERER']);
+}
+
+$id = Input::getvar('id','ARRAYOFINT');
+if (!$id) {
+	CMS::Instance()->queue_message('Cannot perform action on unknown items','danger', $_SERVER['HTTP_REFERER']);
+}
+
+if ($action=='toggle') {
+	$result = DB::exec("UPDATE categories SET state = (CASE state WHEN 1 THEN 0 ELSE 1 END) where id=?", array($id[0])); // id always array even with single id being passed
+	if ($result) {
+		CMS::Instance()->queue_message('Toggled state of Category','success', $_SERVER['HTTP_REFERER']);
+	}
+	else {
+		CMS::Instance()->queue_message('Failed to toggle state of Category','danger', $_SERVER['HTTP_REFERER']);
+	}
+}
+
+if ($action=='publish') {
+	$idlist = implode(',',$id);
+	$result = DB::exec("UPDATE categories SET state = 1 where id in ({$idlist})"); 
+	if ($result) {
+		CMS::Instance()->queue_message('Published Categories','success', $_SERVER['HTTP_REFERER']);
+	}
+	else {
+		CMS::Instance()->queue_message('Failed to publish Categories','danger', $_SERVER['HTTP_REFERER']);
+	}
+}
+
+if ($action=='unpublish') {
+	$idlist = implode(',',$id);
+	$result = DB::exec("UPDATE categories SET state = 0 where id in ({$idlist})"); 
+	if ($result) {
+		CMS::Instance()->queue_message('Unpublished Categories','success', $_SERVER['HTTP_REFERER']);
+	}
+	else {
+		CMS::Instance()->queue_message('Failed to unpublish Categories','danger', $_SERVER['HTTP_REFERER']);
+	}
+}
+
+if ($action=='delete') {
+	$idlist = implode(',',$id);
+	$result = DB::exec("DELETE FROM categories where id in ({$idlist})"); 
+	if ($result) {
+		CMS::Instance()->queue_message('Deleted Categories','success', $_SERVER['HTTP_REFERER']);
+	}
+	else {
+		CMS::Instance()->queue_message('Failed to delete Categories','danger', $_SERVER['HTTP_REFERER']);
+	}
+}
+
