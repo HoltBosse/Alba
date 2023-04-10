d3f92df650b98c886968e5522e840d198af88b16
Bob CMS Test <bobmitch@gmail.com>
2023-04-10 10:18:53 -0400

    fix: cleaner logic

--- core/cms.php
+++ core/cms.php
@@ -626,15 +626,12 @@ final class CMS {
 				}
 				if (!$alias) {
 					// magic alias of 'home' used for now - todo: make configurable via config option in future
-					$home_controller = DB::fetch('select content_type from pages where alias="home"')->content_type && -1;
-					if ($home_controller>0) {
-						// no page found, but we have a home controller that may consume extra params (eg. blog)
-						// let it take care of it and generate 404 if necessary
-						$alias = 'home'; // see magic alias comment above for this and query below
-						$query = "select * from pages where parent=-1 and alias='home' and state>0";
-						$page = $this->pdo->query($query)->fetch();
-					}
-					else {
+					// home page has to have a controller and alias of home and at root of pages and published
+					$alias = 'home'; // see magic alias comment above for this and query below
+					$query = "select * from pages where content_type>0 and parent=-1 and alias='home' and state>0";
+					$page = $this->pdo->query($query)->fetch();
+					if (!$page) {
+						// have params, but no page found and/or home has no controller to consume params
 						$this->raise_404();
 					}
 				}
