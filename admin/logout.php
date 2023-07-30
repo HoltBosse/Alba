f10e1ad0bfefc41550aff0756f203d1fae3f0179
workaj <109080500+workaj@users.noreply.github.com>
2023-07-30 12:52:28 -0400

    feat: account for uripath in logout (#294)

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,9 @@
 <?php
 // TODO: make this a controller so we have access to config to make secure redirect
+define ("CMSPATH", $_SERVER['DOCUMENT_ROOT']); //faking this for now
+require_once("../config.php");
 session_start();
 session_destroy();
 $_SESSION = array();
 //print_r ($_SESSION);
-header('Location: ' . "/admin");
+header('Location: ' . Config::uripath() . "/admin");
