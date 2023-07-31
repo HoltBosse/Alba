0156847158fa5c84e4f6b37cb178d2cd86bb1b46
workaj <109080500+workaj@users.noreply.github.com>
2023-07-31 15:15:27 -0400

    feat: protect the admin home directory (#296)

--- admin/admin_config.php
+++ admin/admin_config.php
@@ -5,6 +5,7 @@ defined('CMSPATH') or die; // prevent unauthorized access
 class Admin_Config {
 
     static $access = [
+        "" => [1,2],
         "content" => [1, 2],
         "tags" => [1, 2],
         "categories" => [1, 2],
