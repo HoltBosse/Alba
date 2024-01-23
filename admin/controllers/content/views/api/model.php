0cafef840f22e66cd9c52e6e200bbd06aedb90f1
workaj <109080500+workaj@users.noreply.github.com>
2024-01-23 08:16:51 -0500

    bug: fix ordering to be compatible with flat alba (#311)

--- admin/controllers/content/views/api/model.php
+++ admin/controllers/content/views/api/model.php
@@ -10,28 +10,30 @@ if ($action=='insert') {
 	$source_id = Input::getvar('sourceid','INT');
 	$dest_id = Input::getvar('destid','INT');
 	$insert_position = Input::getvar('insert_position','STRING');
+	$content_type = Input::getvar('content_type','INT');
+	$content_table = Content::get_table_name_for_content_type($content_type);
 	
 	// source ordering
-	$source_ordering = DB::fetch("select ordering from content where id=?", [$source_id])->ordering;
+	$source_ordering = DB::fetch("select ordering from `$content_table` where id=?", [$source_id])->ordering;
 	// dest ordering
-	$dest_ordering = DB::fetch("select ordering from content where id=?", [$dest_id])->ordering;
+	$dest_ordering = DB::fetch("select ordering from `$content_table` where id=?", [$dest_id])->ordering;
 
 	// make space in ordering first
 	if ($source_ordering < $dest_ordering && $insert_position=='before') {
-		$space_query = "update content set ordering = ordering -1 where ordering>0 and ordering > ? and ordering < ?";
-		$move_query = "update content set ordering = " . ($dest_ordering-1) . " where id={$source_id}";
+		$space_query = "update `$content_table` set ordering = ordering -1 where ordering>0 and ordering > ? and ordering < ?";
+		$move_query = "update `$content_table` set ordering = " . ($dest_ordering-1) . " where id={$source_id}";
 	}
 	elseif ($source_ordering < $dest_ordering && $insert_position=='after') {
-		$space_query = "update content set ordering = ordering -1 where ordering>0 and ordering > ? and ordering <= ?";
-		$move_query = "update content set ordering = {$dest_ordering} where id={$source_id}";
+		$space_query = "update `$content_table` set ordering = ordering -1 where ordering>0 and ordering > ? and ordering <= ?";
+		$move_query = "update `$content_table` set ordering = {$dest_ordering} where id={$source_id}";
 	}
 	elseif ($source_ordering > $dest_ordering && $insert_position=='before') {
-		$space_query = "update content set ordering = ordering +1 where ordering <= ? and ordering >= ?";
-		$move_query = "update content set ordering = " . $dest_ordering . " where id={$source_id}";
+		$space_query = "update `$content_table` set ordering = ordering +1 where ordering <= ? and ordering >= ?";
+		$move_query = "update `$content_table` set ordering = " . $dest_ordering . " where id={$source_id}";
 	}
 	elseif ($source_ordering > $dest_ordering && $insert_position=='after') {
-		$space_query = "update content set ordering = ordering +1 where ordering <= ? and ordering > ?";
-		$move_query = "update content set ordering = " . ($dest_ordering + 1) . " where id={$source_id}";
+		$space_query = "update `$content_table` set ordering = ordering +1 where ordering <= ? and ordering > ?";
+		$move_query = "update `$content_table` set ordering = " . ($dest_ordering + 1) . " where id={$source_id}";
 	}
 	// make space
 	DB::exec($space_query, [$source_ordering, $dest_ordering]);
