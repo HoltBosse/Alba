a14e13287fa35728559d03e4ee51686b1324f610
workaj <109080500+workaj@users.noreply.github.com>
2024-02-14 05:15:41 +0000

    feat: get the ability to edit the widget config file from the front end

--- plugins/core_frontend_editbutton/plugin_class.php
+++ plugins/core_frontend_editbutton/plugin_class.php
@@ -37,11 +37,19 @@ class Plugin_core_frontend_editbutton extends Plugin {
         $data = $this->get_data();
 
         if($this->validateGroup($data->groupOptionsArray, $data->userGroups)) {
-            $page_contents = "
-                <div class='front_end_edit_wrap' >
-                    <a style='' target='_blank' href='/admin/widgets/edit/{$params[0]->id}'>EDIT &ldquo;" . htmlspecialchars($params[0]->title) . "&rdquo;</a>
-                </div>
-            " . $page_contents;
+            if(Config::enable_expiremental_frontend_edit() ?? false) {
+                $page_contents = "
+                    <div class='front_end_edit_wrap' >
+                        <a class='cfe_widget_edit' data-widgetid='{$params[0]->id}' href='#'>EDIT &ldquo;" . htmlspecialchars($params[0]->title) . "&rdquo;</a>
+                    </div>
+                " . $page_contents;
+            } else {
+                $page_contents = "
+                    <div class='front_end_edit_wrap' >
+                        <a style='' target='_blank' href='/admin/widgets/edit/{$params[0]->id}'>EDIT &ldquo;" . htmlspecialchars($params[0]->title) . "&rdquo;</a>
+                    </div>
+                " . $page_contents;
+            }
             return $page_contents;
         } else {
             return $page_contents;
@@ -56,7 +64,7 @@ class Plugin_core_frontend_editbutton extends Plugin {
                 ?>
                     <style>
                         /* TODO: make this indicator better */
-                        .editable_content {
+                        .cfe_editable_content {
                             /* outline: 1px dashed white; */
                             position: relative;
 
@@ -71,10 +79,23 @@ class Plugin_core_frontend_editbutton extends Plugin {
                                 height: 1rem;
                             }
                         }
+
+                        .cfe_widget_dialog {
+                            max-width: 75%;
+
+                            &::backdrop {
+                                background: rgba(0,0,0,0.8);
+                            }
+
+                            & .cfe_closeme {
+                                float: right;
+                                cursor: pointer;
+                            }
+                        }
                     </style>
                     <script>
                         window.addEventListener("load", (e)=>{
-                            document.querySelectorAll(".editable_content").forEach((el)=>{
+                            document.querySelectorAll(".cfe_editable_content").forEach((el)=>{
                                 el.contentEditable = true;
                             });
 
@@ -82,7 +103,7 @@ class Plugin_core_frontend_editbutton extends Plugin {
                                 //console.log("hiii");
 
                                 const formData = new FormData();
-                                formData.append("cfe_update", true);
+                                formData.append("cfe_content_update", true);
                                 formData.append("cfe_contentid", e.target.dataset.contentid);
                                 formData.append("cfe_contenttype", e.target.dataset.contenttype);
                                 formData.append("cfe_contentfield", e.target.dataset.contentfield);
@@ -108,16 +129,75 @@ class Plugin_core_frontend_editbutton extends Plugin {
                             }
 
                             document.body.addEventListener("focusout", (e)=>{
-                                if(e.target.classList.contains("editable_content")) {
+                                if(e.target.classList.contains("cfe_editable_content")) {
                                     submitContent(e);
                                 }
                             });
 
                             document.body.addEventListener("keydown", (e)=>{
-                                if(e.target.classList.contains("editable_content") && e.keyCode === 13) {
+                                if(e.target.classList.contains("cfe_editable_content") && e.keyCode === 13) {
                                     e.preventDefault();
                                     defocus();
                                     //submitContent(e);
+                                }
+                            });
+
+                            document.body.addEventListener("click", (e)=>{
+                                if(e.target.classList.contains("cfe_widget_edit")) {
+                                    e.preventDefault();
+                                    console.log("widget editable");
+
+                                    const formData = new FormData();
+                                    formData.append("cfe_widget_fields", true);
+                                    formData.append("cfe_widgetid", e.target.dataset.widgetid);
+
+                                    fetch(window.location.href, {
+                                        method: "POST",
+                                        body: formData,
+                                    }).then(response => response.text()).then((data) => {
+                                        //console.log(data);
+
+                                        let dialog = document.createElement("dialog");
+                                        document.body.appendChild(dialog);
+                                        dialog.innerHTML = `
+                                            <h5>${e.target.innerText.replace("EDIT", "EDITING")} <span class="cfe_closeme">X</span></h5>
+                                            <form method="post">
+                                                <input type='hidden' name='cfe_widget_fields_submit' value='true'>
+                                                <input type="hidden" name="cfe_widgetid" value="${e.target.dataset.widgetid}"
+                                                ${data}
+                                                <br><br>
+                                                <button type="submit">Submit</submit>
+                                            </form>
+                                        `;
+
+                                        dialog.classList.add("cfe_widget_dialog");
+
+                                        //fix injected script tags
+                                        Array.from(dialog.querySelectorAll("script")).forEach( oldScriptEl => {
+                                            const newScriptEl = document.createElement("script");
+                                            
+                                            Array.from(oldScriptEl.attributes).forEach( attr => {
+                                                newScriptEl.setAttribute(attr.name, attr.value) 
+                                            });
+                                            
+                                            const scriptText = document.createTextNode(oldScriptEl.innerHTML);
+                                            newScriptEl.appendChild(scriptText);
+                                            
+                                            oldScriptEl.parentNode.replaceChild(newScriptEl, oldScriptEl);
+                                        });
+
+                                        dialog.querySelector(".cfe_closeme").addEventListener("click", (e)=>{
+                                            let parentDialog = e.target.closest("dialog");
+                                            parentDialog.close();
+                                            document.body.removeChild(parentDialog);
+                                            window.location.reload(); //reloading because readding the js would potentially cause issues
+                                        });
+
+                                        dialog.showModal();
+                                    }).catch((e)=>{
+                                        alert("Failed to Load Widget Editor");
+                                        window.location.reload();
+                                    });
                                 }
                             });
                         })
@@ -127,7 +207,7 @@ class Plugin_core_frontend_editbutton extends Plugin {
 
             CMS::Instance()->head_entries[] = $contents;
 
-            if(Input::getvar("cfe_update")) {
+            if(Input::getvar("cfe_content_update")) {
                 ob_get_clean();
 
                 header('Content-Type: application/json; charset=utf-8');
@@ -166,6 +246,74 @@ class Plugin_core_frontend_editbutton extends Plugin {
                     "data"=>(object) [],
                 ]);
             }
+        }
+        
+        if(Input::getvar("cfe_widget_fields")) {
+            ob_get_clean();
+
+            //header('Content-Type: application/json; charset=utf-8');
+
+            $widgetid = Input::getvar("cfe_widgetid");
+
+            if(!$widgetid) {
+                http_response_code(500); //500 so js fetch catch works
+                die;
+            }
+
+            $widget = new Widget();
+            $widget->load($widgetid);
+            $form = new Form($widget->form_data);
+
+            foreach ($widget->options as $option) {
+                //echo "$key => $value\n";
+                $field = $form->get_field_by_name($option->name);
+                if ($field) {
+                    $field->default = $option->value;
+                }
+                else {
+                    // do nothing, leave default from form json
+                }
+            }
+
+            $form->display_front_end();
+
+            die;
+
+        }
+
+        if(Input::getvar("cfe_widget_fields_submit")) {
+            
+            $widgetid = Input::getvar("cfe_widgetid");
+
+            if(!$widgetid) {
+                http_response_code(500); //500 so js fetch catch works
+                die;
+            }
+
+            $widget = new Widget();
+            $widget->load($widgetid);
+            $form = new Form($widget->form_data);
+
+            $form->set_from_submit();
+
+            $options = [];
+            foreach ($form->fields as $option) {
+                $obj = new stdClass();
+                $obj->name = $option->name;
+                $obj->value = $option->default;
+                //$obj->{$option->name} = $option->default;
+                $options[] = $obj;
+            }
+
+            $options_json = json_encode($options);
+
+            DB::exec("UPDATE widgets set options=? where id=?", [$options_json, $widgetid]);
+
+
+
+            header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+            die;
+
         }
 
 
