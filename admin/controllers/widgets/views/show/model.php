143fff17c9852b95a79ba6b7f5351f4cebaf4ae3
workaj <109080500+workaj@users.noreply.github.com>
2024-03-19 10:34:14 -0400

    fix https://github.com/HoltBosse/Alba/issues/314 by flipping widget ordering, clean up code (#321)

--- admin/controllers/widgets/views/show/model.php
+++ admin/controllers/widgets/views/show/model.php
@@ -9,16 +9,14 @@ else {
 
 $widget_type_title="";
 if ($widget_type_id && is_numeric($widget_type_id)) {
-	$widget_type_title = CMS::Instance()->pdo->query('select title from widget_types where id=' . $widget_type_id)->fetch()->title;
+	$widget_type_title = DB::fetch('SELECT title FROM widget_types WHERE id=?', [$widget_type_id])->title;
 }
 
 if (is_numeric($widget_type_id)) {
-	//$all_widgets = CMS::Instance()->pdo->query('select * from widgets where state>=0 and type=' . $widget_type_id)->fetchAll();
-	$all_widgets = DB::fetchall('select * from widgets where state>=0 and type=?', array($widget_type_id));
+	$all_widgets = DB::fetchall('SELECT * FROM widgets WHERE state>=0 AND type=? ORDER BY id DESC', [$widget_type_id]);
 }
 else {
-	//$all_widgets = CMS::Instance()->pdo->query('select * from widgets where state>=0')->fetchAll();
-	$all_widgets = DB::fetchall('select * from widgets where state>=0');
+	$all_widgets = DB::fetchall('SELECT * FROM widgets WHERE state>=0 ORDER BY id DESC');
 }
 
 $all_widget_types = Widget::get_all_widget_types();
