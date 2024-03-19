550f035b558a80d3718919dd6c13ac157be34181
bobmitch <bobmitch@gmail.com>
2024-03-19 16:04:59 -0400

    Userflat (#322)
    * init: table creation and simple migration
    
    * feat: editing, saving, bugfix
    
    * feat: review fixes

--- admin/controllers/users/views/edit/view.php
+++ admin/controllers/users/views/edit/view.php
@@ -106,6 +106,10 @@ function in_group ($group_id, $edit_user) {
 
 	<?php Hook::execute_hook_actions('display_user_fields_form',$edit_user); ?>
 
+	<?php if ($custom_user_fields_form) {
+		$custom_user_fields_form->display_front_end();
+	} ?>
+
 	<div class="clear control">
 		<button type="submit" class="button is-primary">Save</button>
 	</div>
