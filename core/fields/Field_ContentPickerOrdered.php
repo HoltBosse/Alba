7026e1975c5995b048bf57739ed73873c4601a76
bobmitch <bobmitch@gmail.com>
2024-04-10 15:09:25 -0400

    init: searchable contentpickerordered (#329)
    * init: searchable contentpickerordered
    
    * feat: maybe

--- core/fields/Field_ContentPickerOrdered.php
+++ core/fields/Field_ContentPickerOrdered.php
@@ -74,6 +74,10 @@ class Field_ContentPickerOrdered extends Field {
                     .twocol_picker ul li:hover {background-color:#ededed; cursor:pointer;}
 					.twocol_picker li.drag-sort-active {opacity:0.2;}
 					span.right-arrow {opacity:0.2; font-weight:bold;}
+					.pickersearch {margin-bottom:1rem; min-width:16rem;}
+					.contentpicker_search_wrap {display:flex; width:100%; gap:1rem;}
+					.pickersearch_clear { display:inline-flex;height: 3.3em;}
+					.pickersearch_control {width:100%;}
                 </style>
                 
                 <hr>
@@ -82,12 +86,27 @@ class Field_ContentPickerOrdered extends Field {
                 <div class='twocol_picker' id='twocol_picker_<?php echo $this->id;?>' >
                     <div class='twocol_picker_left'>
                         <h4 class='is-title title is-4'>All Items</h4>
+						<?php if ($this->searchable):?>
+						<div class='contentpicker_search_wrap'>
+							<div class="control pickersearch_control">
+								<input id='<?php echo $this->id;?>_search' value="" name="contentpicker_search" class="input pickersearch" type="text" placeholder="Search">
+							</div>
+							<button type='button' class='is-default is-small button pickersearch_clear' >X</button>
+						</div>
+						<?php endif; ?>
                         <div class='twocol_picker_wrap'>
                             <ul class='twocol_picker_ul'>
                                 <?php foreach ($options_all_articles as $item):?>
                                     <li 
-									<?php if (in_array($item->id, $existing_arr)) { echo " style='display:none;' ";} ?>
-                                    class='draggable_content_item twocol_picker_source_item' 
+									<?php if (in_array($item->id, $existing_arr)) { 
+										echo " style='display:none;' ";
+										$picked_class = " picked";
+									}
+									else {
+										$picked_class = "";
+									}
+									?>
+                                    class='draggable_content_item twocol_picker_source_item <?php echo $picked_class; ?>' 
                                     draggable 
                                     data-content_id='<?php echo $item->id;?>'
                                     data-content_title='<?php echo $item->title;?>'
@@ -128,6 +147,46 @@ class Field_ContentPickerOrdered extends Field {
 				<p class='note'>Click items on the left to add to selected area. Drag and drop in selected area to reorder. To remove a selected item, click it.</p>
                 <script>
                     let picker_<?php echo $this->id;?> = document.getElementById('twocol_picker_<?php echo $this->id;?>');
+					// handle search
+					picker_<?php echo $this->id;?>.querySelector('.pickersearch')?.addEventListener('input',function(e){
+						// loop over values that partially match search string
+						let searchstring = e.target.value;
+						if (searchstring) {
+							// filter
+							picker_<?php echo $this->id;?>.querySelectorAll('.twocol_picker_source_item').forEach(el => {
+								if (el.innerText.toLowerCase().includes(searchstring.toLowerCase()) && !el.classList.contains('picked')) {
+									el.style.display = 'block';
+								}
+								else {
+									el.style.display = 'none';
+								}
+							});
+						}
+						else {
+							// show all
+							picker_<?php echo $this->id;?>.querySelectorAll('.twocol_picker_source_item').forEach(el => {
+								if (el.classList.contains('picked')) {
+									el.style.display = 'none';
+								}
+								else {
+									el.style.display = 'block';
+								}
+							});
+						}
+					});
+					// handle clear search
+					picker_<?php echo $this->id;?>.querySelector('.pickersearch_clear')?.addEventListener('click',function(e){
+						e.target.closest('.contentpicker_search_wrap').querySelector('.pickersearch').value = '';
+						// show all
+						picker_<?php echo $this->id;?>.querySelectorAll('.twocol_picker_source_item').forEach(el => {
+							if (el.classList.contains('picked')) {
+								el.style.display = 'none';
+							}
+							else {
+								el.style.display = 'block';
+							}
+						});
+					});
 					// apply drag drop to server rendered lis
 					let rendered_lis_<?php echo $this->id;?> = picker_<?php echo $this->id;?>.querySelectorAll('.twocol_picker_right ul li');
 					rendered_lis_<?php echo $this->id;?>.forEach(li => {
@@ -203,6 +262,7 @@ class Field_ContentPickerOrdered extends Field {
 							hidden_input.value = csv_arr.join(",");
 							// hide original clicked element - ready to restore if clicked in right column
 							e.target.style.display='none';
+							e.target.classList.add('picked');
                         }
 						else {
 							if (e.target.nodeName=="LI") {
@@ -223,8 +283,17 @@ class Field_ContentPickerOrdered extends Field {
 								let picker = e.target.closest('.twocol_picker');
 								let left_col_el = picker.querySelector('.twocol_picker_left li[data-content_id="' + id + '"]');
 								left_col_el.style.display = 'block';
+								left_col_el.classList.remove('picked');
 								// remove right hand element, no longer needed
 								e.target.remove();
+								// check if matches filter
+								let searchstring = picker_<?php echo $this->id;?>.querySelector('.pickersearch')?.value;
+								if (searchstring) {
+									if (!left_col_el.innerText.toLowerCase().includes(searchstring.toLowerCase()) ) {
+										// no match - hide
+										left_col_el.style.display = 'none';
+									}
+								}
 							}
 						}
                     });
@@ -256,6 +325,7 @@ class Field_ContentPickerOrdered extends Field {
 		$this->tags = $config->tags ?? [];
 		$this->list_unpublished = $config->list_unpublished ?? false;
 		$this->logic = $config->logic ?? '';
+		$this->searchable = $config->searchable ?? true;
 	}
 
 	public function get_friendly_value($helpful_info) {
