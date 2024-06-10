2f8c283d5877fc4c3e979c3aa1c97d788fccd567
Bob CMS Test <bobmitch@gmail.com>
2024-06-10 10:19:11 -0400

    feat: better redirects controller arg checking

--- admin/controllers/redirects/controller.php
+++ admin/controllers/redirects/controller.php
@@ -6,11 +6,19 @@ defined('CMSPATH') or die; // prevent unauthorized access
 $segments = CMS::Instance()->uri_segments;
 $content_type_filter=null;
 
-if (sizeof($segments)==1) {
+if (sizeof($segments)==0) {
+	CMS::Instance()->queue_message('Unknown redirect operation','danger', Config::uripath() . '/admin');
+}
+elseif (sizeof($segments)==1) {
 	$view = 'all';
 }
 else {
-	$view = $segments[1];
+	if (is_dir(CMSPATH . "/admin/controllers/redirects/views/" . $segments[1])) {
+		$view = $segments[1];
+	}
+	else {
+		CMS::Instance()->queue_message('Unknown redirect view','danger', Config::uripath() . '/admin');
+	}
 }
 
 $content_type_controller = new Controller(realpath(dirname(__FILE__)),$view);
