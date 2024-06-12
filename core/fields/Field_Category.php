fb89ad4bfebf88bfde293612796fe88498e9a755
workaj <109080500+workaj@users.noreply.github.com>
2024-06-11 22:39:18 -0400

    chore: convert category field to use oop

--- core/fields/Field_Category.php
+++ core/fields/Field_Category.php
@@ -1,81 +1,36 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
 
-class Field_Category extends Field {
+class Field_Category extends Field_Select {
 
-	public $select_options;
 	public $self_id;
 
-	function __construct($tagid="") {
-		$this->id = "";
-		$this->name = "";
-		$this->default = 0;
-		$this->content_type="1";
-		$this->self_id; // prevent showing self as option for parent
-	}
+	public function load_from_config($config) {
+		parent::load_from_config($config);
 
-	
+		$this->content_type = $config->content_type ?? 1;
+	}
 
 	public function display() {
-		$required="";
-		if ($this->content_type) {
-			$cats = Content::get_applicable_categories ($this->content_type);
+		//we have to do this here, rather than the config method as the property is set o nthe field after the config is loaded
+		$query = "SELECT id AS value, title as text FROM categories WHERE content_type=?";
+		$params = [$this->content_type];
+
+		if($this->self_id) {
+			$query .= " AND id!=?";
+			$params[] = $this->self_id;
 		}
-		if ($this->required) {$required=" required ";}
-		echo "<div class='field'>";
-			echo "<label class='label'>" . $this->label . "</label>";
-			echo "<div class='control'>";
-				echo "<div class='select'>";
-					echo "<select {$required} id='{$this->id}' {$this->get_rendered_name()}>";
-						if ($this->required) {
-							echo "<option value='' >{$this->label}</option>";
-						}
-						else {
-							echo "<option value='0'>None</option>";
-						}
-						foreach ($cats as $cat) {
-							if ($this->self_id==$cat->id) {
-								// don't show self if self_id is set
-								continue;
-							}
-							if ($cat->state==1) {
-								$selected = "";
-								if ($cat->id == $this->default) { $selected="selected";}
-								echo "<option {$selected} value='{$cat->id}'>{$cat->title}</option>";
-							}
-						}
-					echo "</select>";
-				echo "</div>";
-			echo "</div>";
-		echo "</div>";
-		if ($this->description) {
-			echo "<p class='help'>" . $this->description . "</p>";
-		}
-	}
 
+		$this->select_options = DB::fetchall($query, $params);
 
-	public function load_from_config($config) {
-		$this->name = $config->name ?? 'error!!!';
-		$this->id = $config->id ?? $this->name;
-		$this->label = $config->label ?? '';
-		$this->required = $config->required ?? false;
-		$this->description = $config->description ?? '';
-		$this->filter = $config->filter ?? 'NUMBER';
-		$this->missingconfig = $config->missingconfig ?? false;
-		$this->default = $config->default ?? '';
-		$this->type = $config->type ?? 'error!!!';
-		$this->content_type = $config->content_type ?? 1;
-		$this->logic = $config->logic ?? '';
-	}
+		array_unshift(
+			$this->select_options,
+			(object) [
+				"text"=>"None",
+				"value"=>0,
+			]
+		);
 
-	public function get_friendly_value($helpful_info) {
-		return DB::fetch('select title from categories where id=?', [$this->default])->title;
-	}
-
-	public function validate() {
-		if ($this->is_missing()) {
-			return false;
-		}
-		return true;
+		parent::display();
 	}
 }
