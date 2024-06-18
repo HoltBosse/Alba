6a65159454bf8287ce2f7061f36e953f3df1decb
workaj <109080500+workaj@users.noreply.github.com>
2024-06-18 13:01:28 -0400

    feat: add pagination

--- admin/controllers/audit/views/default/model.php
+++ admin/controllers/audit/views/default/model.php
@@ -1,7 +1,17 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
 
-//TODO: glob /core/actions, and limit this query to the support actions type. also add pagination
-$results = DB::fetchall("SELECT * FROM user_actions ORDER BY date DESC");
+$cur_page = $_GET["page"] ?? 1;
+$pagination_size = Configuration::get_configuration_value ('general_options', 'pagination_size') ?? 10;
+
+$actionClasses = glob(CMSPATH . "/core/actions/*.php");
+$actionTypes = [];
+foreach($actionClasses as $class) {
+    $file = basename($class);
+    $actionTypes[] = explode(".", (explode("_", $file)[1]))[0];
+}
+$actionTypesString = "'" . implode("','", $actionTypes) . "'";
+
+$results = DB::fetchall("SELECT * FROM user_actions WHERE `type` IN ({$actionTypesString}) ORDER BY date DESC LIMIT {$pagination_size} OFFSET " . (($cur_page-1)*$pagination_size));
 
 //CMS::pprint_r($results);
