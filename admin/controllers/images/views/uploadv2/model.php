324ad0e658942e3c036fc9262968289e5da8e596
workaj <109080500+workaj@users.noreply.github.com>
2024-07-01 23:56:42 -0400

    chore: comment out worlds most pointless if statement

--- admin/controllers/images/views/uploadv2/model.php
+++ admin/controllers/images/views/uploadv2/model.php
@@ -57,32 +57,32 @@ function make_thumb($src, $dest, $desired_width, $file) {
 
 function correctImageOrientation($filename) {
 	if (function_exists('exif_read_data')) {
-	  $exif = exif_read_data($filename);
-	  if($exif && isset($exif['Orientation'])) {
-		$orientation = $exif['Orientation'];
-		if($orientation != 1 || true){
-		  $img = imagecreatefromjpeg($filename);
-		  $deg = 0;
-		  switch ($orientation) {
-			case 3:
-			  $deg = 180;
-			  break;
-			case 6:
-			  $deg = 270;
-			  break;
-			case 8:
-			  $deg = 90;
-			  break;
-		  }
-		  if ($deg) {
-			$img = imagerotate($img, $deg, 0);        
-		  }
-		  // then rewrite the rotated image back to the disk as $filename 
-		  imagejpeg($img, $filename, 75);
-		} // if there is some rotation necessary
-	  } // if have the exif orientation info
+		$exif = exif_read_data($filename);
+		if($exif && isset($exif['Orientation'])) {
+			$orientation = $exif['Orientation'];
+			/* if($orientation != 1 || true){ */
+				$img = imagecreatefromjpeg($filename);
+				$deg = 0;
+				switch ($orientation) {
+					case 3:
+						$deg = 180;
+						break;
+					case 6:
+						$deg = 270;
+						break;
+					case 8:
+						$deg = 90;
+						break;
+				}
+				if ($deg) {
+					$img = imagerotate($img, $deg, 0);        
+				}
+				// then rewrite the rotated image back to the disk as $filename 
+				imagejpeg($img, $filename, 75);
+			/* } */ // if there is some rotation necessary
+		} // if have the exif orientation info
 	} // if function exists  
-  }
+}
 
 // upload images to processed directory - make web friendly if needed
 $n=0;
