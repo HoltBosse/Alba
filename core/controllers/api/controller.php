9bfc7cc042669640d79cab281b2c6cb142dad1d5
workaj <109080500+workaj@users.noreply.github.com>
2024-07-15 15:38:57 -0400

    feat: lock down api to authenticated users

--- core/controllers/api/controller.php
+++ core/controllers/api/controller.php
@@ -2,14 +2,6 @@
 defined('CMSPATH') or die; // prevent unauthorized access
 header('Content-Type: application/json; charset=utf-8');
 
-$markup = '
-Welcome to the demo:
-
-1. Write Markdown text on the left
-2. Hit the __Parse__ button or ***~~thing~~***
-3. See the result to on the right
-';
-
 $response = [
     "data"=>[],
     "success"=>false,
@@ -19,6 +11,11 @@ $response = [
 $segments = CMS::Instance()->uri_segments;
 
 if(sizeof($segments)==2 && $segments[1]=="parsedown") {
+
+    //user must be logged in to use this api, to prevent against abuse
+    if(CMS::Instance()->user->id===false) {
+        echo json_encode($response);
+    }
 
     if($_GET["markup"]) {
 
