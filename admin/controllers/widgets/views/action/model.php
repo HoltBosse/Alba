02999578d6de0cd274686bbd7b43ea6f83750e2c
janessalee11 <119462355+janessalee11@users.noreply.github.com>
2024-07-25 12:14:09 -0400

    feat: links added for publishing/unpublishing widgets

--- admin/controllers/widgets/views/action/model.php
+++ admin/controllers/widgets/views/action/model.php
@@ -1,58 +1,32 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
 
+$actions = [
+	"toggle" => ["(CASE state WHEN 1 THEN 0 ELSE 1 END)", "toggled"],
+	"publish" => [1, "published"],
+	"unpublish" => [0, "unpublished"],
+	"delete" => [-1, "deleted"],
+];
+
 $action = CMS::Instance()->uri_segments[2];
-if (!$action) {
-	CMS::Instance()->queue_message('Unknown action','danger', $_SERVER['HTTP_REFERER']);
-}
-
 $id = Input::getvar('id','ARRAYOFINT');
-if (!$id) {
-	CMS::Instance()->queue_message('Cannot perform action on unknown items','danger', $_SERVER['HTTP_REFERER']);
+if (!$action || !$id || !$actions[$action]) {
+	CMS::Instance()->queue_message('Unknown action or items','danger', $_SERVER['HTTP_REFERER']);
 }
 
-if ($action=='toggle') {
-	$result = DB::exec("UPDATE widgets SET state = (CASE state WHEN 1 THEN 0 ELSE 1 END) where id=?", array($id[0])); // id always array even with single id being passed
-	if ($result) {
-		$widget = DB::fetch('SELECT * FROM widgets WHERE id=?', [$id[0]]);
-		$msg = "Widget <a href='" . Config::uripath() . "/admin/widgets/edit/{$id[0]}'>{$widget->title}</a> state toggled";
-		CMS::Instance()->queue_message($msg,'success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to toggle state of widget','danger', $_SERVER['HTTP_REFERER']);
-	}
+function exec_action ($state, $action_text, $ids) {
+	$injectionString = $ids ? implode(",", array_map(function($input) { return "?"; }, $ids)) : "?";
+	$result = DB::exec("UPDATE widgets SET state = $state WHERE id IN ($injectionString)", $ids);
+
+	if(!$result) { CMS::Instance()->queue_message('Failed to complete action','danger', $_SERVER['HTTP_REFERER']); }
+
+	$widgets = DB::fetchall("SELECT * FROM widgets WHERE id in ($injectionString)", $ids);
+	$widgetsMsgString = implode(", ", array_map(function($input) { return "<a target='_blank' href='" . Config::uripath() . "/admin/widgets/edit/$input->id'>$input->title</a>"; }, $widgets));
+
+	CMS::Instance()->queue_message("Widget(s) " . ($state!=-1 ? $widgetsMsgString : "") . " $action_text",'success', $_SERVER['HTTP_REFERER']);
+
 }
 
-if ($action=='publish') {
-	$idlist = implode(',',$id);
-	$result = DB::exec("UPDATE widgets SET state = 1 where id in ({$idlist})"); 
-	if ($result) {
-		CMS::Instance()->queue_message('Published widgets','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to publish widgets','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
-
-if ($action=='unpublish') {
-	$idlist = implode(',',$id);
-	$result = DB::exec("UPDATE widgets SET state = 0 where id in ({$idlist})"); 
-	if ($result) {
-		CMS::Instance()->queue_message('Unpublished widgets','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to unpublish widgets','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
-
-if ($action=='delete') {
-	$idlist = implode(',',$id);
-	$result = DB::exec("UPDATE widgets SET state = -1 where id in ({$idlist})"); 
-	if ($result) {
-		CMS::Instance()->queue_message('Deleted widgets','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to delete widgets','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
+$actionDetails = $actions[$action];
+exec_action($actionDetails[0], $actionDetails[1], $id);
 
