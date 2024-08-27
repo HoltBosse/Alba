1d95b6aa0022538d42f5d7bd0641168743df81c9
workaj <109080500+workaj@users.noreply.github.com>
2024-08-27 11:24:25 -0400

    chore: make phpstan happy

--- plugins/core_widget/plugin_class.php
+++ plugins/core_widget/plugin_class.php
@@ -12,32 +12,32 @@ class Plugin_core_widget extends Plugin {
         $matches = [];
         preg_match_all("^\{{widget (.*?)\}}^", $page_contents, $matches, PREG_PATTERN_ORDER);
         //CMS::pprint_r ($matches);
-        if ($matches) {
-            for ($n=0; $n<sizeof($matches[0]); $n++) {
-                $widget_markup = ""; // default to blank string
-                $widget_snippet = $matches[0][$n];
-                $widget_id = $matches[1][$n];
-                $widget = DB::fetch('select * from widgets where id=? and state>0',$widget_id);
-                // if widget loaded correctly
-                if ($widget) {
-                    // create widget obj
-                    $type_info = Widget::get_widget_type($widget->type);
-				    $widget_class_name = "Widget_" . $type_info->location;
-				    $widget_of_type = new $widget_class_name();
-				    $widget_of_type->load ($widget->id);
-                    // output widget into buffer and store
-                    ob_start();
-                    $widget_of_type->internal_render();
-                    $widget_markup = ob_get_contents();
-                    ob_end_clean();
-                }
-                else {
-                    $widget_markup = "<p class='note'>Unable to load widget {$widget_id}</p>";
-                }
-                $page_contents = str_replace($widget_snippet, $widget_markup, $page_contents);
-                //$page_contents = str_replace("<p>" . $widget_snippet . "</p>", $widget_markup, $page_contents);
+        
+        for ($n=0; $n<sizeof($matches[0]); $n++) {
+            $widget_markup = ""; // default to blank string
+            $widget_snippet = $matches[0][$n];
+            $widget_id = $matches[1][$n];
+            $widget = DB::fetch('select * from widgets where id=? and state>0',$widget_id);
+            // if widget loaded correctly
+            if ($widget) {
+                // create widget obj
+                $type_info = Widget::get_widget_type($widget->type);
+                $widget_class_name = "Widget_" . $type_info->location;
+                $widget_of_type = new $widget_class_name();
+                $widget_of_type->load ($widget->id);
+                // output widget into buffer and store
+                ob_start();
+                $widget_of_type->internal_render();
+                $widget_markup = ob_get_contents();
+                ob_end_clean();
             }
+            else {
+                $widget_markup = "<p class='note'>Unable to load widget {$widget_id}</p>";
+            }
+            $page_contents = str_replace($widget_snippet, $widget_markup, $page_contents);
+            //$page_contents = str_replace("<p>" . $widget_snippet . "</p>", $widget_markup, $page_contents);
         }
+
         return $page_contents;
     }
 }
