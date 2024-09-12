6e2109c65d272f243a7212f65058f2a54d9d98b9
workaj <109080500+workaj@users.noreply.github.com>
2024-09-11 23:40:35 -0400

    chore: convert raw xhr to fetch, get rid of duplicate postajax function (#398)

--- admin/controllers/images/views/show/script.js
+++ admin/controllers/images/views/show/script.js
@@ -227,22 +227,6 @@ function clear_selection() {
 	});
 }
 
-function postAjax(url, data, success) {
-	var params = typeof data == 'string' ? data : Object.keys(data).map(
-			function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-		).join('&');
-
-	var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-	xhr.open('POST', url);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
-	};
-	xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send(params);
-	return xhr;
-}
-
 function clear_tags() {
 	ids = get_selected_ids();
 	if (ids.length>0) {
