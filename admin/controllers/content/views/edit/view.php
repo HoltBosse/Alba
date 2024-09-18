d507cb2e77ec24b360331216a88d60d7e695e158
workaj <109080500+workaj@users.noreply.github.com>
2024-09-18 08:56:23 -0400

    feat: implementation of warn before closing when a field is edited (#403)
    * feat: implementation of warn before closing when a field is edited
    
    * chore: clear up spam
    
    * chore: fix/improve logic

--- admin/controllers/content/views/edit/view.php
+++ admin/controllers/content/views/edit/view.php
@@ -14,7 +14,27 @@ defined('CMSPATH') or die; // prevent unauthorized access
 	
 <?php endif; ?>
 
+<script>
+	function unloadCheckerFunction(e) {
+		let blockStatus = false;
+		document.querySelectorAll("input, textarea").forEach((i)=>{
+			if(i.value != (i.getAttribute("value") ?? "")) {
+				blockStatus = true;
+			}
+		});
 
+		if(blockStatus) {
+			e.preventDefault();
+			e.returnValue = true;
+		}
+	}
+	window.addEventListener("beforeunload", unloadCheckerFunction);
+	window.addEventListener("load", ()=>{
+		document.querySelector("form").addEventListener("submit", ()=>{
+			window.removeEventListener("beforeunload", unloadCheckerFunction);
+		});
+	});
+</script>
 
 <hr>
 
