b03bdf3f6848130fb8f510dc8ff24b3c4b9d7f23
workaj <109080500+workaj@users.noreply.github.com>
2024-09-25 13:50:47 -0400

    feat: get template switching completely working

--- admin/controllers/pages/views/graphicaledit/script.js
+++ admin/controllers/pages/views/graphicaledit/script.js
@@ -5,12 +5,25 @@ window.addEventListener("load", ()=>{
         source: "graphicaledit",
     });
 
-    document.querySelector("iframe").contentWindow.postMessage({
+    document.querySelector("#template").addEventListener("change", (e)=>{
+        console.log(document.querySelector("#template").value);
+
+        document.querySelector("iframe").contentWindow.postMessage({
+            message: {
+                action: "changeTemplate",
+                params: [document.querySelector("#template").value]
+            },
+            type: "command",
+            source: "graphicaledit",
+        });
+    });
+
+    /* document.querySelector("iframe").contentWindow.postMessage({
         message: {
             action: "changeTemplate",
             params: ["bulma_graphicaledit"]
         },
         type: "command",
         source: "graphicaledit",
-    });
+    }); */
 });
