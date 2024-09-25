affcbdde4d9027147dfc8d42a2cd65021fcdd150
workaj <109080500+workaj@users.noreply.github.com>
2024-09-25 14:42:38 -0400

    chore: markup mockup

--- admin/controllers/pages/views/graphicaleditrender/view.php
+++ admin/controllers/pages/views/graphicaleditrender/view.php
@@ -15,11 +15,20 @@ ob_start();
                 <?php echo file_get_contents(CMSPATH . "/admin/controllers/pages/views/graphicaleditrender/commands.js"); ?>
                 <?php echo file_get_contents(CMSPATH . "/admin/controllers/pages/views/graphicaleditrender/script.js"); ?>
             </script>
+            <section style="border: 1px dashed; padding: 1rem;">
+                <div>
+                    <p style="background-color: red; text-align: center; margin-bottom: 0;">EXISTING CONTENT HERE</p>
+                </div>
+                <br>
+                <div style="display: flex; justify-content: center;">
+                    <span style="border: 1px solid; border-radius: 20rem; height: 2rem; width: 2rem; text-align: center;">+</span>
+                </div>
+            </section>
+            <br>
+            <div style="display: flex; justify-content: center;">
+                <span style="border: 1px solid; border-radius: 2rem; white-space: pre;">  + new widget +  </span>
+            </div>
         <?php
-        for ($i = 1; $i <= 20; $i++) {
-            CMS::pprint_r("some rando content");
-            echo "<br><br><br><br><br><br>";
-        }
         CMS::jprint_r("test");
     echo "<section>";
 $pageContent = ob_get_clean();
