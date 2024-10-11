344b927599ea8dbf017c32d32010caa7f0356f85
Bob CMS Test <bobmitch@gmail.com>
2024-10-11 15:51:08 -0400

    init: repeatable friendly checkbox

--- core/fields/Field_Checkbox.php
+++ core/fields/Field_Checkbox.php
@@ -1,46 +1,32 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
 
+// by using a select input to handle the actual submitted values
+// the checkbox field can be natively used to handle the UX accessibly
+// this will force EVERY checked field to submit a value 
+// - this is needed for correct evaluation of checkbox form data in a repeatable
+
 class Field_Checkbox extends Field {
 
 	public function display() {
 		echo "<div class='field'>";
-			echo "<label for='{$this->id}' class='checkbox'>";
-				echo "<input type='hidden' value='0' {$this->get_rendered_name()}>"; // ensure submitted value
-				$required="";
-				if ($this->required) {$required=" required ";}
-				if ($this->default) {$checked=" checked=checked ";} // 0 value stored for unchecked
-				echo "<input $checked value='1' type='checkbox' id='{$this->id}' {$this->get_rendered_name()}>";
+			$required="";
+			if ($this->required) {$required=" required ";}
+			// hidden select
+			$off_selected = $this->default ? "" : " selected ";
+			$on_selected = $this->default ? " selected " : "";
+			echo "<select style='display:none;' id='{$this->id}' {$this->get_rendered_name()}><option $off_selected value='0'>Off</option><option $on_selected value='1'>On</option></select>";
+			// visible checkbox + label
+			echo "<label for='checkbox_{$this->id}{{repeatable_id_suffix}}' class='checkbox'>";
+				if ($this->default) {$checked=" checked=checked ";} 
+				$onchange_js = 'this.closest(".field").querySelector("select").value=(this.checked ? 1 : 0);'; // set option to match checkbox
+				echo "<input onchange='$onchange_js' $checked value='1' type='checkbox' id='checkbox_{$this->id}{{repeatable_id_suffix}}' >"; // no name given, not submitted
 				echo "&nbsp;" . $this->label;
 			echo "</label>";
 			if ($this->description) {
 				echo "<p class='help'>" . $this->description . "</p>";
 			}
 		echo "</div>";
-	}
-
-
-	public function inject_designer_javascript() {
-		?>
-		<script>
-			window.Field_Checkbox = {};
-			// template is what gets injected when the field 'insert new' button gets clicked
-			window.Field_Text.designer_template = `
-			<div class="field">
-				<h2 class='heading title'>Text Field</h2>	
-
-				<label class="label">Label</label>
-				<div class="control has-icons-left has-icons-right">
-					<input required name="label" class="input iss-success" type="label" placeholder="Label" value="">
-				</div>
-
-				<label class="label">Required</label>
-				<div class="control has-icons-left has-icons-right">
-					<input name="required" class="checkbox iss-success" type="checkbox"  value="">
-				</div>
-			</div>`;
-		</script>
-		<?php 
 	}
 
 	public function get_friendly_value($helpful_info) {
