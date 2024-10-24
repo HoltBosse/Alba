5338430db9891a5b07b9887555691cbc16bd6c70
janessalee11 <119462355+janessalee11@users.noreply.github.com>
2024-10-24 16:51:38 -0400

    feat: ensure no duplicates in tagged table

--- installer/schema.sql
+++ installer/schema.sql
@@ -103,8 +103,9 @@ DROP TABLE IF EXISTS `tagged`;
 CREATE TABLE `tagged` (
   `tag_id` int(11) NOT NULL,
   `content_id` int(11) NOT NULL,
-  `content_type_id` int(11) NOT NULL COMMENT 'Important: -1 signifies MEDIA, -2 signifies USERS'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `content_type_id` int(11) NOT NULL COMMENT 'Important: -1 signifies MEDIA, -2 signifies USERS',
+  UNIQUE KEY `tag_id_content_id_content_type_id` (`tag_id`,`content_id`,`content_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `tags`;
 CREATE TABLE `tags` (
