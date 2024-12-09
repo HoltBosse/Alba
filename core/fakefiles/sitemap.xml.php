07ba474f6685e46171b08688118b14e6da1d615d
workaj <109080500+workaj@users.noreply.github.com>
2024-12-09 13:55:50 -0500

    chore: refactor

--- core/fakefiles/sitemap.xml.php
+++ core/fakefiles/sitemap.xml.php
@@ -1,15 +1,9 @@
 <?php
+defined('CMSPATH') or die; // prevent unauthorized access
 
 header("Content-type: text/xml");
 
-echo '<?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-';
-
+$sitemap_data = [];
 $pages = Page::get_all_pages_by_depth();
 foreach($pages as $page) {
     if($page->state<=0) {
@@ -25,10 +19,28 @@ foreach($pages as $page) {
         $priority = $priority + (substr_count($path, '/') * -0.1);
     }
 
+    $sitemap_data[] = [
+        "loc"=>$_SERVER["SERVER_NAME"] . $path,
+        "lastmod"=>"2024-08-23T12:02:33+00:00",
+        "priority"=>$priority,
+    ];
+}
+
+//TODO: add hook here so more can be injected
+
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+';
+
+foreach($sitemap_data as $page_item) {
     echo "<url>";
-        echo "<loc>https://" . $_SERVER["SERVER_NAME"] . $path . "</loc>";
-        echo "<lastmod>2024-08-23T12:02:33+00:00</lastmod>";
-        echo "<priority>$priority</priority>";
+        foreach($page_item as $element=>$item) {
+            echo "<{$element}>{$item}</{$element}>";
+        }
     echo "</url>";
 }
 
