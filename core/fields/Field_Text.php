97d034de60b9faf1e3489e82df834d8e4a961702
workaj <109080500+workaj@users.noreply.github.com>
2025-01-02 16:39:43 -0500

    Revert from add slashes to htmlspecial chars, remove filter in content title that causes conflicts (#438)
    * Revert "feat: fix text field apostrophe catastrophe (#424)"
    
    This reverts commit c74be3a9d582aff7f381b53c548d3d1eba73ff84.
    
    * bug: filters are garbage - mixing the string filter with htmlspecial chars breaks things

--- core/fields/Field_Text.php
+++ core/fields/Field_Text.php
@@ -55,7 +55,7 @@ class Field_Text extends Field {
 					$step=" step='{$this->step}' ";
 				}
 				$placeholder = $this->placeholder ?? "";
-				$value = addslashes($this->default);
+				$value = htmlspecialchars($this->default);
 				echo "<input type='{$this->input_type}' value='{$value}' placeholder='{$placeholder}' {$minmax} {$pattern} {$step} {$this->get_rendered_name()} maxlength={$this->maxlength} minlength={$this->minlength} class='filter_{$this->filter} input' {$required} type='text' id='{$this->id}' >";
 			echo "</div>";
 			if ($this->description) {
