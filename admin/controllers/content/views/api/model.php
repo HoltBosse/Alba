b5bd5e379522c039c5fe8b608222cf62bb6fbfa9
Bob CMS Test <bobmitch@gmail.com>
2025-02-16 11:57:21 -0500

    fix: changeorder logic bug

--- admin/controllers/content/views/api/model.php
+++ admin/controllers/content/views/api/model.php
@@ -81,9 +81,7 @@ elseif ($action=="changeorder") {
 		exit(0);
 	}
 	$content_table = Content::get_table_name_for_content_type($content_type);
-	// First, update the ordering index of the item being moved
 	$stmt = DB::exec("UPDATE $content_table SET ordering = ? WHERE id = ?",[$new_order, $id]);
-	// Then, update the ordering indices of the other items
 	if ($new_order > $prev_order) {
 		// Item was moved down, so decrement the ordering indices of the items above it
 		DB::exec("UPDATE $content_table SET ordering = ordering - 1 WHERE ordering > ? AND ordering <= ?", [$prev_order, $new_order]);
@@ -91,6 +89,8 @@ elseif ($action=="changeorder") {
 		// Item was moved up, so increment the ordering indices of the items below it
 		DB::exec("UPDATE $content_table SET ordering = ordering + 1 WHERE ordering >= ? AND ordering < ?",[$new_order, $prev_order]);
 	}
+	// Update moved items ordering
+	$stmt = DB::exec("UPDATE $content_table SET ordering = ? WHERE id = ?",[$new_order, $id]);
 	echo '{"success":1,"message":"Ordering complete"}';
 	exit(0);
 }
