de61fe9e36045d8fbdbc1e02f8bd9cc1fce8c153
Bob CMS Test <bobmitch@gmail.com>
2025-02-17 16:47:30 -0500

    chore: final review changes, external js etc

--- admin/controllers/content/views/order/view.php
+++ admin/controllers/content/views/order/view.php
@@ -1,9 +1,5 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
-
-// TODO: handle custom states - see all view
-// TODO: handle base grid column count - see all view
-
 ?>
 
 <style>
@@ -16,12 +12,8 @@ defined('CMSPATH') or die; // prevent unauthorized access
             cursor:move;
         }
     }
-    .sortable-ghost {
-    }
     .sortable-chosen {
         font-weight:bold;
-    }
-    .sortable-drag {
     }
     table#orderitems tr {
         grid-template-columns: repeat(<?php echo sizeof($content_list_fields)+4;?>, 1fr); /* match # of columns in ordering table */
@@ -102,43 +94,7 @@ defined('CMSPATH') or die; // prevent unauthorized access
 
 <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.6/Sortable.min.js"></script>
 <script>
-    new Sortable(document.getElementById('ordertablebody'), {
-        animation: 150,
-        onUpdate: (e)=>{
-            /* console.log("From ", e.oldIndex, " to ", e.newIndex);
-            console.log(e.item.dataset); */
-            // update all ordering indices to ensure correctness
-            // get arr of ids
-            let id_arr=[];
-            let content_type=<?=$content_type;?>;
-            document.querySelectorAll('.orderitem').forEach((e)=>{
-                id_arr.push(e.dataset.content_id);
-            });
-            let id_arr_string = id_arr.join(",");
-            if (id_arr_string) {
-                // pass to server
-                console.log(id_arr_string);
-                
-                async function update_ordering() {
-                    const formData = new FormData();
-                    // changeorder
-                    // id, new_order, prev_order, content_type
-                    formData.append("action", "changeorder");
-                    formData.append("id", e.item.dataset.content_id);
-                    formData.append("content_type", <?=$content_type;?>);
-                    formData.append("new_order", e.newIndex+1); // client-size index 0 based, ordering 1
-                    formData.append("prev_order", e.oldIndex+1);
-                    fetch("<?=Config::uripath();?>/admin/content/api", {
-                            method: "POST",
-                            body: formData,
-                    }).then((response) => response.json()).then((data) => {
-                        console.log(data);
-                    });
-                }
-
-                update_ordering();
-            }
-        },
-    });
+    let content_type=<?=$content_type;?>;
+    <?php echo file_get_contents(CMSPATH . "/admin/controllers/content/views/order/script.js"); ?>
 </script>
 
