59bde8cba582db495d295336551f8d3362d4d3d6
Bob CMS Test <bobmitch@gmail.com>
2025-02-17 16:52:02 -0500

    chore: remove unused code make biome happy

--- admin/controllers/content/views/order/script.js
+++ admin/controllers/content/views/order/script.js
@@ -3,35 +3,24 @@ new Sortable(document.getElementById('ordertablebody'), {
     onUpdate: (e)=>{
         /* console.log("From ", e.oldIndex, " to ", e.newIndex);
         console.log(e.item.dataset); */
-        // update all ordering indices to ensure correctness
-        // get arr of ids
-        let id_arr=[];
-        document.querySelectorAll('.orderitem').forEach((e)=>{
-            id_arr.push(e.dataset.content_id);
-        });
-        let id_arr_string = id_arr.join(",");
-        if (id_arr_string) {
-            // pass to server
-            console.log(id_arr_string);
-            
-            async function update_ordering() {
-                const formData = new FormData();
-                // changeorder
-                // id, new_order, prev_order, content_type
-                formData.append("action", "changeorder");
-                formData.append("id", e.item.dataset.content_id);
-                formData.append("content_type", content_type);
-                formData.append("new_order", e.newIndex+1); // client-size index 0 based, ordering 1
-                formData.append("prev_order", e.oldIndex+1);
-                fetch(window.uripath + "/admin/content/api", {
-                        method: "POST",
-                        body: formData,
-                }).then((response) => response.json()).then((data) => {
-                    console.log(data);
-                });
-            }
-
-            update_ordering();
+        async function update_ordering() {
+            const formData = new FormData();
+            // changeorder
+            // id, new_order, prev_order, content_type
+            formData.append("action", "changeorder");
+            formData.append("id", e.item.dataset.content_id);
+            formData.append("content_type", content_type);
+            formData.append("new_order", e.newIndex+1); // client-size index 0 based, ordering 1
+            formData.append("prev_order", e.oldIndex+1);
+            fetch(`${window.uripath}/admin/content/api`, {
+                    method: "POST",
+                    body: formData,
+            }).then((response) => response.json()).then((data) => {
+                console.log(data);
+            });
         }
+
+        update_ordering();
+        
     },
 });
