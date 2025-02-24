3dabe538687131d474eab04a81b5c3a2bccca0b5
workaj <109080500+workaj@users.noreply.github.com>
2025-02-24 14:50:04 -0500

    Build improvements (#476)
    * feat: improve build bundle - remove file from git for next commit
    
    * chore: add removed file to git ignore - now handled by composer install
    
    * chore: remove new admin config file

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -9,12 +9,14 @@ rm -rf user_classes
 rm -rf .gitignore
 rm -rf README.md
 rm -rf configTESTBACKUP.php
+rm -rf configADMINTESTBACKUP.php
 rm -rf home.svg
 rm -rf htaccess.txt
-rm -rf index.php
 rm -rf testform.json
 rm -rf testrepeatform.json
 rm -rf setupfiles.sh
+rm -rf biome.json
+rm -rf phpstan.neon
 rm -rf .git
 rm -rf .github
 echo "finished removing fluff"
