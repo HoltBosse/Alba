b8220caeaa953606aa4ca14879fa7a1d2a09a1bd
workaj <109080500+workaj@users.noreply.github.com>
2025-03-20 14:26:41 -0400

    feat: start adding some additional required fields

--- admin/controllers/forms/views/edit/model.php
+++ admin/controllers/forms/views/edit/model.php
@@ -1,2 +1,7 @@
 <?php
 defined('CMSPATH') or die; // prevent unauthorized access
+
+$requiredFieldsObject = json_decode(file_get_contents(CMSPATH . "/admin/controllers/content/views/edit/required_fields_form.json"));
+$requiredFieldsForm = new Form($requiredFieldsObject);
+
+$formOptionsForm = new Form(CMSPATH . "/admin/controllers/forms/views/edit/form_options.json");
--- admin/controllers/forms/views/edit/view.php
+++ admin/controllers/forms/views/edit/view.php
@@ -7,60 +7,81 @@ $fieldsList = array_merge($coreFields, $userFields);
 //CMS::pprint_r($fieldsList);
 ?>
 
-<style>
-    <?php echo file_get_contents(CMSPATH . "/admin/controllers/forms/views/edit/style.css"); ?>
-</style>
+<form>
+    <style>
+        <?php
+            echo file_get_contents(CMSPATH . "/admin/controllers/forms/views/edit/style.css");
+            echo file_get_contents(CMSPATH . "/admin/controllers/content/views/edit/style.css");
+        ?>
+    </style>
 
-<h1 class="title">New Form</h1>
+    <h1 class="title">New Form</h1>
 
-<section class="form_editor_wrapper">
-    <div class="controls_panel">
-        <div class="controls_button_fields_grid">
-            <label for="add_field" class="button is-primary">Add Field</label>
-            <input id="add_field" type="radio" style="display: none;" name="controls_pane_option" checked>
-            <label for="field_settings" class="button is-primary" disabled>Field Settings</label>
-            <input id="field_settings" type="radio" style="display: none;" name="controls_pane_option">
-        </div>
-        <hr>
-        <div class="add_field_list controls_button_fields_grid">
-            <?php
-                foreach($fieldsList as $fieldPath) {
-                    $className = basename($fieldPath, ".php");
-                    $type = str_replace("Field_", "", $className);
-                    $classInstance = new $className();
+    <hr>
 
-                    if($classInstance->get_form_editor_visibility()) {
-                        $markup = htmlspecialchars($classInstance->get_form_editor_display());
-                        $config = json_encode($classInstance->get_form_editor_config());
-                        //$markup = "<p>$type</p>";
-                        echo "<a
-                                class='button is-info'
-                                data-display_markup='$markup'
-                                data-type='$type'
-                                data-config='$config'
-                            >
-                            $type
-                            </a>";
+    <div class='flex'>
+        <?php
+            $requiredFieldsForm->display_front_end();
+        ?>
+    </div>
+
+    <hr>
+
+    <?php
+        $formOptionsForm->display_front_end();
+    ?>
+
+    <hr>
+
+    <section class="form_editor_wrapper">
+        <div class="controls_panel">
+            <div class="controls_button_fields_grid">
+                <label for="add_field" class="button is-primary">Add Field</label>
+                <input id="add_field" type="radio" style="display: none;" name="controls_pane_option" checked>
+                <label for="field_settings" class="button is-primary" disabled>Field Settings</label>
+                <input id="field_settings" type="radio" style="display: none;" name="controls_pane_option">
+            </div>
+            <hr>
+            <div class="add_field_list controls_button_fields_grid">
+                <?php
+                    foreach($fieldsList as $fieldPath) {
+                        $className = basename($fieldPath, ".php");
+                        $type = str_replace("Field_", "", $className);
+                        $classInstance = new $className();
+
+                        if($classInstance->get_form_editor_visibility()) {
+                            $markup = htmlspecialchars($classInstance->get_form_editor_display());
+                            $config = json_encode($classInstance->get_form_editor_config());
+                            //$markup = "<p>$type</p>";
+                            echo "<a
+                                    class='button is-info'
+                                    data-display_markup='$markup'
+                                    data-type='$type'
+                                    data-config='$config'
+                                >
+                                $type
+                                </a>";
+                        }
                     }
-                }
-            ?>
+                ?>
+            </div>
+            <div class="field_configuration_list">
+                <p>fields config select</p>
+            </div>
         </div>
-        <div class="field_configuration_list">
-            <p>fields config select</p>
+        <div class="fields_panel">
+            
         </div>
-    </div>
-    <div class="fields_panel">
-        
-    </div>
-</section>
-<input name="form_json" type="text" style="display: none;">
-<br><br>
+    </section>
+    <input name="form_json" type="text" style="display: none;">
+    <br><br>
 
-<div class="fixed-control-bar">
-	<button title="Save and exit" class="button is-primary" type="submit">Save</button>
-	<button class="button is-warning" type="button" onclick="window.history.back();">Cancel</button>
-</div>
+    <div class="fixed-control-bar">
+        <button title="Save and exit" class="button is-primary" type="submit">Save</button>
+        <button class="button is-warning" type="button" onclick="window.history.back();">Cancel</button>
+    </div>
 
-<script>
-    <?php echo file_get_contents(CMSPATH . "/admin/controllers/forms/views/edit/script.js"); ?>
-</script>
+    <script>
+        <?php echo file_get_contents(CMSPATH . "/admin/controllers/forms/views/edit/script.js"); ?>
+    </script>
+</form>
