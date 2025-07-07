72c99c75aeec7414a67c987d0f57e26d538af9cb
workaj <109080500+workaj@users.noreply.github.com>
2025-07-07 13:56:35 -0400

    chore: unify tag fields (#553)

--- core/fields/Field_TagSingle.php
+++ core/fields/Field_TagSingle.php
@@ -3,6 +3,33 @@ defined('CMSPATH') or die; // prevent unauthorized access
 
 class Field_TagSingle extends Field_Select {
 
+	public $tag_cache = [];
+
+	private function get_parent_tag($input) {
+		if ($input->parent != 0) {
+			if($this->tag_cache[$input->parent]) {
+				return $this->tag_cache[$input->parent];
+			} else {
+				$parent_tag = DB::fetch("SELECT * FROM tags WHERE id=?", $input->parent);
+				$this->tag_cache[$parent_tag->id] = $parent_tag;
+				return $parent_tag;
+			}
+		} else {
+			return (object) ["parent"=>0];
+		}
+	}
+
+	private function make_tag_path($input) {
+		$title = $input->title;
+		while($input->parent != 0) {
+			$parent_tag = $this->get_parent_tag($input);
+			$title = $parent_tag->title . " > " . $title;
+			$input = $parent_tag;
+		}
+
+		return $title;
+	}
+
 	public function loadFromConfig($config) {
 		parent::loadFromConfig($config);
 
@@ -18,7 +45,7 @@ class Field_TagSingle extends Field_Select {
 		$this->select_options = [];
 		foreach($tags as $tag) {
 			$this->select_options[] = (object) [
-				"text"=>$tag->title,
+				"text"=>$this->make_tag_path($tag),
 				"value"=>$tag->id,
 				"disabled"=>$tag->state ? false : true
 			];
