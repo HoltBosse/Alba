0a8f98130709aa499847f1e92b8855c10c3099c8
workaj <109080500+workaj@users.noreply.github.com>
2025-11-04 15:12:40 -0500

    trashfire: toss last remenants of content_versions (#658)

--- bin/sql/mariadb/content_versions.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `content_versions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `content_id` int NOT NULL,
-  `created_by` int NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `fields_json` mediumtext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
