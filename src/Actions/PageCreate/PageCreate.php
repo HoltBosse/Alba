80855fbea8b794cec36e8408bd1ae951dd08919b
workaj <109080500+workaj@users.noreply.github.com>
2025-11-19 14:50:42 -0500

    bug: safely handle text in created page action

--- src/Actions/PageCreate/PageCreate.php
+++ src/Actions/PageCreate/PageCreate.php
@@ -3,12 +3,13 @@ namespace HoltBosse\Alba\Actions\PageCreate;
 
 use HoltBosse\Alba\Core\Actions;
 use HoltBosse\DB\DB;
+use HoltBosse\Form\Input;
 
 class PageCreate extends Actions {
 
     public function display() {
         $affectedPageDetails = DB::fetch("SELECT * FROM pages WHERE id=?", $this->options->affected_page);
 
-        $this->render_row(null, "Created Page: $affectedPageDetails->title");
+        $this->render_row(null, "Created Page: " . Input::stringHtmlSafe($affectedPageDetails->title));
     }
 }
