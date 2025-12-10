73cf1368afba4a1a3be0edd3ddaa6806986e1fb8
workaj <109080500+workaj@users.noreply.github.com>
2025-12-10 09:25:33 -0500

    trashfire: toss dead api methods

--- src/admin/controllers/images/views/api/model.php
+++ src/admin/controllers/images/views/api/model.php
@@ -126,41 +126,6 @@ if ($action=='untag_media') {
 	exit(0);
 }
 
-if ($action=='toggle') {
-	// NOT APPLICABLE TO IMAGES!
-	$result = DB::exec("UPDATE tags SET state = (CASE state WHEN 1 THEN 0 ELSE 1 END) where id=?", [$id[0]]); // id always array even with single id being passed
-	if ($result) {
-		CMS::Instance()->queue_message('Toggled state of tag','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to toggle state of tag','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
-
-if ($action=='publish') {
-	// NOT APPLICABLE TO IMAGES!
-	$idlist = implode(',',$id);
-	$result = DB::exec("UPDATE tags SET state = 1 where id in ({$idlist})"); 
-	if ($result) {
-		CMS::Instance()->queue_message('Published tags','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to publish tags','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
-
-if ($action=='unpublish') {
-	// NOT APPLICABLE TO IMAGES!
-	$idlist = implode(',',$id);
-	$result = DB::exec("UPDATE tags SET state = 0 where id in ({$idlist})"); 
-	if ($result) {
-		CMS::Instance()->queue_message('Unpublished tags','success', $_SERVER['HTTP_REFERER']);
-	}
-	else {
-		CMS::Instance()->queue_message('Failed to unpublish tags','danger', $_SERVER['HTTP_REFERER']);
-	}
-}
-
 if ($action=='delete') {
 	$idlist = implode(',',$id);
 	
