f97a5217322c6415ac5bc692078061d8a294014f
workaj <109080500+workaj@users.noreply.github.com>
2026-01-26 13:32:56 -0500

    feat: break up image api into progamatic chunks

--- src/Core/Image.php
+++ src/Core/Image.php
@@ -95,4 +95,143 @@ class Image {
             return $markup;
         }
     }
+
+    public static function makeThumb(string $src, string $dest, int $desired_width, File $file): bool {
+        /* read the source image */
+        if ($file->mimetype=='image/jpeg') {
+            $source_image = imagecreatefromjpeg($src);
+        } elseif ($file->mimetype=='image/webp') {
+            $source_image = imagecreatefromwebp($src);
+        } else {
+            $source_image = imagecreatefrompng($src);
+        }
+        $width = imagesx($source_image);
+        $height = imagesy($source_image);
+        /* find the "desired height" of this thumbnail, relative to the desired width  */
+        $desired_height = (int) floor($height * ($desired_width / $width)); //floor returns a float, cast to int, cause php is weird
+        /* create a new, "virtual" image */
+        $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+        /* copy source image at a resized size */
+        imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+        /* create the physical thumbnail image to its destination */
+        if ($file->mimetype=='image/jpeg') {
+            return imagejpeg($virtual_image, $dest);
+        } elseif ($file->mimetype=='image/webp') {
+            return imagewebp($virtual_image, $dest);
+        } else {
+            return imagepng($virtual_image, $dest);
+        }
+    }
+
+    public static function correctImageOrientation(File $file): bool {
+        if($file->mimetype != 'image/jpeg') {
+            throw new Exception('Image orientation correction only available for JPEG images');
+        }
+
+        $filename = $file->filepath;
+
+        if (function_exists('exif_read_data')) {
+            $exif = exif_read_data($filename);
+            if($exif && isset($exif['Orientation'])) {
+                $orientation = $exif['Orientation'];
+                /* if($orientation != 1 || true){ */
+                    $img = imagecreatefromjpeg($filename);
+                    $deg = 0;
+                    switch ($orientation) {
+                        case 3:
+                            $deg = 180;
+                            break;
+                        case 6:
+                            $deg = 270;
+                            break;
+                        case 8:
+                            $deg = 90;
+                            break;
+                    }
+                    if ($deg) {
+                        $img = imagerotate($img, $deg, 0);        
+                    }
+                    // then rewrite the rotated image back to the disk as $filename 
+                    return imagejpeg($img, $filename, 75);
+                /* } */ // if there is some rotation necessary
+            } // if have the exif orientation info
+        } // if function exists  
+        return false;
+    }
+
+    public static function processUploadedFiles(array $files, array $alts, array $titles, array $tags, array $web_friendly_array, string $directory): object {
+        $image_types_data = File::$image_types;
+        $uploaded_files = [];
+        $n=0;
+        $img_ids = [];
+        foreach ($files["error"] as $key => $error) {
+            if ($error == UPLOAD_ERR_OK) {
+                $tmp_name = $files["tmp_name"][$key];
+                // basename() may prevent filesystem traversal attacks;
+                // further validation/sanitation of the filename may be appropriate
+                // make filename unique-ish
+                // TODO: per user images or per-date a la wordpress for unode / performance reasons?
+                $name = uniqid() . "_" . basename($files["name"][$key]);
+                $dest = $directory . "/" . $name;
+                move_uploaded_file($tmp_name, $dest);
+                $file = new File($dest);
+                if($file->mimetype=="image/jpeg") {
+                    Image::correctImageOrientation($file);
+                }
+                $uploaded_files[] = $name;
+                //  get file info and put in db
+                $title = $titles[$n];
+                $alt = $alts[$n];
+                $web_friendly = $web_friendly_array[$n];
+                // TODO: further filtering on title and alt just in case
+                if ($file->width > 1920 && $web_friendly[$n]) {
+                    $file->original_width = $file->width;
+                    $file->recalc_height(1920);
+                }
+                $in_db_ok = DB::exec(
+                    "INSERT INTO media (width, height, title, alt, filename, mimetype, domain) VALUES (?,?,?,?,?,?,?)",
+                    [$file->width, $file->height, $title, $alt, $file->filename, $file->mimetype, ($_SESSION["current_domain"] ?? CMS::getDomainIndex($_SERVER['HTTP_HOST']))]
+                );
+                $img_ids[] = DB::getLastInsertedId();
+                if ($in_db_ok) {
+                    $thumbdest = $_ENV["images_directory"] . '/processed/' . "web_" . $file->filename;
+                    // make web friendly if required
+                    if ($file->original_width > 1920 && $web_friendly[$n] && $image_types_data[$file->mimetype]==1) {
+                        Image::makeThumb($dest, $thumbdest, 1920, $file);
+                        //unlink($src);
+                    }
+                    //$processed[] = $all_image_files[$n];
+
+                    foreach(json_decode($tags[$n]) as $tag_id) {
+                        DB::exec("insert into tagged (content_id, tag_id, content_type_id) values (?,?,-1)", [$img_ids[sizeof($img_ids)-1], $tag_id]);
+                    }
+                }
+                else {
+                    // TODO: handle db insert error - shouldn't happen, but need to return an appropriate JSON string to be handled
+                }
+            }
+            $n++;
+        }
+        $uploaded_files = implode(",",$uploaded_files);
+
+        foreach($img_ids as $id) {
+            Actions::add_action("mediacreate", (object) [
+                "affected_media"=>$id,
+            ]);
+        }
+
+
+        // return json to javascript uploaded and
+        CMS::Instance()->queue_message('Images uploaded','success');
+        //echo '{"success":1,"msg":"Images uploaded","files":"'.$uploaded_files.'","ids":"'.implode(",",$img_ids).'"}';
+
+        return (object) [
+            "success"=>1,
+            "msg"=>"Images uploaded",
+            "files"=>$uploaded_files,
+            "ids"=>implode(",",$img_ids),
+            "tags"=>json_encode($tags),
+            "urls"=>implode(",", array_map(function($c){ return $_ENV["uripath"] . "/image/$c"; }, $img_ids)),
+        ];
+    }
 }
