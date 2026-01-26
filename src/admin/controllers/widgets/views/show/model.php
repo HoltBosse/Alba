68c78d29c19ffd3ad97900375e8c2733b7bd8046
workaj <109080500+workaj@users.noreply.github.com>
2026-01-26 15:52:09 -0500

    bug: only show widgets with valid types by default

--- src/admin/controllers/widgets/views/show/model.php
+++ src/admin/controllers/widgets/views/show/model.php
@@ -77,7 +77,7 @@ if($queryResult->results !== null && $queryResult->totalCount !== null) {
 		$widget_type_title = DB::fetch('SELECT title FROM widget_types WHERE id=?', [$widget_type_id])->title;
 	}
 	
-	$query = 'SELECT w.* FROM widgets w LEFT JOIN widget_types wt ON w.type=wt.id WHERE w.state>=0 AND (domain IS NULL OR domain=?)';
+	$query = 'SELECT w.* FROM widgets w LEFT JOIN widget_types wt ON w.type=wt.id WHERE w.state>=0 AND (domain IS NULL OR domain=?) AND w.type IN (SELECT id FROM widget_types)';
 	$params = [$_SESSION["current_domain"]];
 	
 	if(is_numeric($widget_type_id)) {
