5b46607800bedfae04a051d38ffff004df86c0cb
workaj <109080500+workaj@users.noreply.github.com>
2026-01-25 21:27:43 -0500

    bug: fix unrelated page title rendering

--- src/Actions/PageUpdate/PageUpdate.php
+++ src/Actions/PageUpdate/PageUpdate.php
@@ -3,12 +3,13 @@ namespace HoltBosse\Alba\Actions\PageUpdate;
 
 use HoltBosse\DB\DB;
 use HoltBosse\Alba\Core\Actions;
+use HoltBosse\Form\Input;
 
 class PageUpdate extends Actions {
 
     public function display() {
         $affectedPageDetails = DB::fetch("SELECT * FROM pages WHERE id=?", $this->options->affected_page);
 
-        $this->render_row(null, "Updated Page: $affectedPageDetails->title");
+        $this->render_row(null, "Updated Page: " . Input::stringHtmlSafe($affectedPageDetails->title));
     }
 }
