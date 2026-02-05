b3c3e55140b09fb1b62909c02bacab3519b223d4
workaj <109080500+workaj@users.noreply.github.com>
2026-02-04 23:10:42 -0500

    feat: togglable password field

---
+++ src/Fields/ToggablePassword/ToggablePassword.php
@@ -0,0 +1,49 @@
+<?php
+namespace HoltBosse\Alba\Fields\ToggablePassword;
+
+use HoltBosse\Form\Fields\Input\Input as Text;
+
+class ToggablePassword extends Text {
+    public function display() {
+        parent::display();
+        ?>
+            <style>
+                .field_id_<?php echo $this->id; ?> span.is-right {
+                    pointer-events: auto !important;
+                    cursor: pointer;
+
+                    * {
+                        pointer-events: none !important;
+                    }
+                }
+            </style>
+            <script type='module'>
+                document.querySelector('.field_id_<?php echo $this->id; ?> span.is-right').addEventListener('click', (e)=>{
+                    const input = document.getElementById('<?php echo $this->id; ?>');
+                    if (input.type === 'password') {
+                        input.type = 'text';
+                    } else {
+                        input.type = 'password';
+                    }
+
+                    const icon = e.target.querySelector("i");
+                    icon.classList.toggle('fa-eye');
+                    icon.classList.toggle('fa-eye-slash');
+                });
+            </script>
+        <?php
+    }
+
+    public function loadFromConfig($config) {
+        parent::loadFromConfig($config);
+        
+        $this->input_type = $config->input_type ?? 'password';
+        $this->icon_status = $config->icon_status ?? true;
+
+        $this->icon_parent_class = $config->icon_parent_class ?? "";
+        $this->icon_parent_class = $this->icon_parent_class . " has-icons-right";
+
+        $this->icon_markup = $config->icon_markup ?? "";
+        $this->icon_markup = $this->icon_markup . "<span class='icon is-small is-right'><i class='fas fa-eye'></i></span>";
+    }
+}
