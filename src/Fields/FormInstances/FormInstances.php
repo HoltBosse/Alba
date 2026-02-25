4f401ff744836bd141c826254e2b0f537eee1e23
workaj <109080500+workaj@users.noreply.github.com>
2026-02-25 09:29:19 -0500

    chore: multidomain support in forms widget

---
+++ src/Fields/FormInstances/FormInstances.php
@@ -0,0 +1,24 @@
+<?php
+namespace HoltBosse\Alba\Fields\FormInstances;
+
+Use HoltBosse\Form\Fields\Select\Select;
+Use HoltBosse\Alba\Core\{CMS};
+Use HoltBosse\Alba\Fields\SqlSelector\SqlSelector;
+
+class FormInstances extends SqlSelector {
+    public ?int $domain = null;
+
+	public function loadFromConfig(object $config): self {
+        $domain = (CMS::Instance()->isAdmin() ? $_SESSION["current_domain"] : CMS::getDomainIndex($_SERVER["HTTP_HOST"])) ?? CMS::getDomainIndex($_SERVER["HTTP_HOST"]);
+
+        $this->domain = $config->domain ?? $domain;
+        
+		$config->query = "SELECT id as value, title as text FROM `form_instances` WHERE state >= 0 AND (domain = ? OR domain IS NULL) ORDER BY title ASC";
+		$config->params = [$this->domain];
+
+        parent::loadFromConfig($config);
+
+		return $this;
+	}
+
+}
